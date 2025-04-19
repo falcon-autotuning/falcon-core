545e6b01ae4ebb67594cf5cdc44c800aa46bb1bb
tjk144 <tkovach@wisc.edu>
2025-04-19 14:45:08 -0500

    Fixed the coupled labelled domain to not be Jsonable

--- src/falcon_core/math/domains/coupled_labelled_domain.py
+++ src/falcon_core/math/domains/coupled_labelled_domain.py
@@ -1,10 +1,11 @@
 """A collection of coupled labelled domains to be attached together."""
 
 from .base_coupled_labelled_domain import BaseCoupledLabelledDomain
-from .dependancies import Generic, Jsonable, TypeVar
+from .dependancies import Generic, TypeVar
+from .typing import Any
 
-T = TypeVar("T", bound=Jsonable)
+T = TypeVar("T", bound=Any)
 
 
-class CoupledLabelledDomain(BaseCoupledLabelledDomain, Jsonable, Generic[T]):
+class CoupledLabelledDomain(BaseCoupledLabelledDomain, Generic[T]):
     """A collection of coupled labelled domains to be attached together."""
