f9e0612c15394c8f5aa68f219d2242e2f7a13d72
tjk144 <tkovach@wisc.edu>
2025-04-19 14:46:29 -0500

    Added descriptions to the coupled labelled domain

--- src/falcon_core/math/domains/coupled_labelled_domain.py
+++ src/falcon_core/math/domains/coupled_labelled_domain.py
@@ -8,4 +8,8 @@ T = TypeVar("T", bound=Any)
 
 
 class CoupledLabelledDomain(BaseCoupledLabelledDomain, Generic[T]):
-    """A collection of coupled labelled domains to be attached together."""
+    """A collection of coupled labelled domains to be attached together.
+
+    These labelled domains are not intended to be passed over the communication channels.
+    They can be used to label any domain within the API space.
+    """
