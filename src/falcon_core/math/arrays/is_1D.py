5efef4d82173f156751b23332d1874b9994a875f
jvlad1979 <daschug1@gmail.com>
2025-07-16 20:00:19 -0400

    added more robust 1d coercion

--- src/falcon_core/math/arrays/is_1D.py
+++ src/falcon_core/math/arrays/is_1D.py
@@ -61,8 +61,11 @@ class Is1D:
             raise TypeError(msg)
 
         if not self.is_1D:
-            msg = f"Cannot convert non-1D array to 1D. Got shape {self.shape}"
-            raise ValueError(msg)
+            try:
+                self._data = self.data.reshape(self.shape[0])
+            except:
+                msg = f"Cannot convert non-1D array to 1D (even after attempted reshape). Got shape {self.shape}"
+                raise ValueError(msg)
 
         return cast(array1D, self.data)
 
