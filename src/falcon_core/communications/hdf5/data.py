9ffd8f1d9d898e14006f9055f7ff4b506885a3d9
tjk144 <tkovach@wisc.edu>
2025-07-18 22:55:35 -0500

    Added a print to the dimensions

--- src/falcon_core/communications/hdf5/data.py
+++ src/falcon_core/communications/hdf5/data.py
@@ -357,6 +357,7 @@ class HDF5Data(Jsonable):
 
         obj = cls.__new__(cls)
         obj._dimensions = dimensions
+        print(f"The dimensions are {dimensions}")
         obj._domains = domains
         obj._ranges = ranges
         obj._metadata = metadata
