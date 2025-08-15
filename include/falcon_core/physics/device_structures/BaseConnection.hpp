7a2fe41bf7987eae1f7529d0b27bac3a6bae96b1
jvlad1979 <daschug1@gmail.com>
2025-08-15 17:12:51 -0400

    chore: fixed garbage in base connection

--- include/falcon_core/physics/device_structures/BaseConnection.hpp
+++ include/falcon_core/physics/device_structures/BaseConnection.hpp
@@ -6,17 +6,8 @@ namespace falcon_core {
 namespace physics {
 namespace device_structures {
 class BaseConnection : public generic::Song {
-<<<<<<< HEAD
- public:
-  BaseConnection(std::string name) : _name(std::move(name)) {}
-  std::string name() const { return _name; }
-
-  bool operator<(const BaseConnection& other) const { return _name < other._name; }
-
- private:
-=======
   std::string _name;
->>>>>>> 1128ada7a7e3e226ca56a6a13f15884266037517
+
   template <class Archive>
   void serialize(Archive &ar) {
     ar(cereal::base_class<Song>(this), _name);
