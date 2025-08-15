7799097a8c9c02fdb4d349e22e5cca49e6c1afbe
tjk144 <tkovach@wisc.edu>
2025-08-14 21:46:26 -0500

    The quantity and the axes are now available?

--- include/falcon_core/math/Axes.hpp
+++ include/falcon_core/math/Axes.hpp
@@ -1,37 +1,26 @@
 #pragma once
 
-#include <memory>
-#include <vector>
-
 #include "falcon_core/generic/Song.hpp"
 
 namespace falcon_core {
 namespace math {
 
 template <typename T>
-class Axes : public generic::Song {
+class Axes : public std::vector<std::shared_ptr<T>>, public generic::Song {
  public:
-  using value_type     = std::shared_ptr<T>;
-  using container_type = std::vector<value_type>;
-
-  Axes() = default;
-  explicit Axes(container_type items) : _items(std::move(items)) {}
-
-  void append(const value_type &item) { _items.push_back(item); }
-
-  const value_type &at(size_t index) const { return _items.at(index); }
-
-  size_t size() const { return _items.size(); }
-
-  bool empty() const { return _items.empty(); }
-
-  typename container_type::const_iterator begin() const {
-    return _items.begin();
-  }
-  typename container_type::const_iterator end() const { return _items.end(); }
+  using std::vector<std::shared_ptr<T>>::vector;
 
  private:
-  container_type _items;
+  template <class Archive>
+  void serialize(Archive& ar) {
+    ar(cereal::base_class<generic::Song>(this),
+       cereal::base_class<std::vector<std::shared_ptr<T>>>(this));
+  }
+
+ protected:
+  Axes() = default;
+  friend class cereal::access;
 };
+
 }  // namespace math
 }  // namespace falcon_core
