8e4fca4bfcce3e1e47843cac6ffbf2e0715312d2
jvlad1979 <daschug1@gmail.com>
2025-08-15 18:44:59 -0400

    fix: Modify Eigen serialization for compatibility with JSON archives

--- include/cereal/types/eigen.hpp
+++ include/cereal/types/eigen.hpp
@@ -2,6 +2,7 @@
 
 #include <cereal/cereal.hpp>
 #include <Eigen/Dense>
+#include <cereal/types/vector.hpp>
 
 namespace cereal
 {
@@ -9,24 +10,16 @@ namespace cereal
   template <class Archive, class _Scalar, int _Rows, int _Cols, int _Options, int _MaxRows, int _MaxCols> inline
   void save(Archive & ar, Eigen::Matrix<_Scalar, _Rows, _Cols, _Options, _MaxRows, _MaxCols> const & m)
   {
-    int32_t rows = m.rows();
-    int32_t cols = m.cols();
-    ar(rows);
-    ar(cols);
-    ar(binary_data(m.data(), rows * cols * sizeof(_Scalar)));
+    ar( m.rows(), m.cols(), std::vector<_Scalar>(m.data(), m.data() + m.size()) );
   }
 
   //! Loading for Eigen::Matrix
   template <class Archive, class _Scalar, int _Rows, int _Cols, int _Options, int _MaxRows, int _MaxCols> inline
   void load(Archive & ar, Eigen::Matrix<_Scalar, _Rows, _Cols, _Options, _MaxRows, _MaxCols> & m)
   {
-    int32_t rows;
-    int32_t cols;
-    ar(rows);
-    ar(cols);
-
-    m.resize(rows, cols);
-
-    ar(binary_data(m.data(), static_cast<std::size_t>(rows) * static_cast<std::size_t>(cols) * sizeof(_Scalar)));
+    Eigen::Index rows, cols;
+    std::vector<_Scalar> data;
+    ar( rows, cols, data );
+    m = Eigen::Map<const Eigen::Matrix<_Scalar, Eigen::Dynamic, Eigen::Dynamic>>(data.data(), rows, cols);
   }
 }
