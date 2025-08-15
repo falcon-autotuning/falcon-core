3074e203edcd8369efbaf17574c7750fc34ef6dc
tjk144 <tkovach@wisc.edu>
2025-08-14 22:31:44 -0500

    The test is updated to match the old tests

--- tests/test_unit.cpp
+++ tests/test_unit.cpp
@@ -1,8 +1,9 @@
 #include <gtest/gtest.h>
 
 #include "falcon_core/Constants.hpp"
+#include "falcon_core/physics/units/CommonUnits.hpp"
+#include "falcon_core/physics/units/TotalDimensions.hpp"
 #include "falcon_core/physics/units/Unit.hpp"
-
 using namespace falcon_core;
 using namespace falcon_core::physics::units;
 
@@ -12,92 +13,189 @@ class UnitTest : public ::testing::Test {
   TotalDimensions dim_len_  = {{SI::DIMENSION_LENGTH, 1}};
   TotalDimensions dim_time_ = {{SI::DIMENSION_TIME, 1}};
 };
-
-TEST_F(UnitTest, ConstructionAndAccessors) {
-  Unit meter(dim_len_, 1.0, 0.0, "");
-  ASSERT_EQ(meter.dimensions(), dim_len_);
+/**
+ * @brief test that the base SI units are correctly defined.
+ */
+TEST(TestCommonUnits, BaseUnits) {
+  Unit            meter = common_units::Meter;
+  TotalDimensions mdims = {{SI::DIMENSION_LENGTH, 1}};
+  ASSERT_EQ(meter.dimensions(), mdims);
   ASSERT_DOUBLE_EQ(meter.scale_factor(), 1.0);
-  ASSERT_DOUBLE_EQ(meter.offset(), 0.0);
-  ASSERT_EQ(meter.prefix(), "");
+
+  Unit            kilogram = common_units::Kilogram;
+  TotalDimensions k_dims   = {{SI::DIMENSION_MASS, 1}};
+  ASSERT_EQ(kilogram.dimensions(), k_dims);
+
+  Unit            ampere = common_units::Ampere;
+  TotalDimensions a_dims = {{SI::DIMENSION_CURRENT, 1}};
+  ASSERT_EQ(ampere.dimensions(), a_dims);
+
+  Unit            second = common_units::Second;
+  TotalDimensions s_dims = {{SI::DIMENSION_TIME, 1}};
+  ASSERT_EQ(second.dimensions(), s_dims);
 }
 
-TEST_F(UnitTest, Compatibility) {
-  Unit meter(dim_len_);
-  Unit second(dim_time_);
-  Unit foot(dim_len_, 0.3048);
+TEST(TestCommonUnits, DerivedUnits) {
+  Unit volt                               = common_units::Volt;
+  TotalDimensions expected_volt_dims = {
+      {SI::DIMENSION_MASS, 1},
+      {SI::DIMENSION_LENGTH, 2},
+      {SI::DIMENSION_TIME, -3},
+      {SI::DIMENSION_CURRENT, -1},
+  }; 
+  ASSERT_EQ(volt.dimensions(), expected_volt_dims);
 
-  ASSERT_TRUE(meter.is_compatible_with(foot));
-  ASSERT_FALSE(meter.is_compatible_with(second));
+  Unit newton                               = common_units::Newton;
+  TotalDimensions expected_newton_dims = {
+      {SI::DIMENSION_MASS, 1},
+      {SI::DIMENSION_LENGTH, 1},
+      {SI::DIMENSION_TIME, -2},
+  };
+  ASSERT_EQ(newton.dimensions(), expected_newton_dims);
+
+  Unit hertz                               = common_units::Hertz;
+  TotalDimensions expected_hertz_dims = {
+      {SI::DIMENSION_TIME, -1},
+  };
+  ASSERT_EQ(hertz.dimensions(), expected_hertz_dims);
+
+  Unit tesla                               = common_units::Tesla;
+  TotalDimensions expected_tesla_dims = {
+      {SI::DIMENSION_MASS, 1},
+      {SI::DIMENSION_TIME, -2},
+      {SI::DIMENSION_CURRENT, 1},
+  };
+  ASSERT_EQ(tesla.dimensions(), expected_tesla_dims);
 }
 
-TEST_F(UnitTest, Conversion) {
-  Unit meter(dim_len_);
-  Unit kilometer(dim_len_, 1000.0);
-  Unit second(dim_time_);
+TEST(TestCommonUnits, ConsistencyWithDefinitions) {
+  // Joule = Newton * Meter
+  Unit joule = common_units::Joule;
+  Unit newton_meter = common_units::Newton * common_units::Meter;
+  ASSERT_EQ(joule.dimensions(), newton_meter.dimensions());
 
-  ASSERT_DOUBLE_EQ(meter.convert_value_to(1000, kilometer), 1.0);
-  ASSERT_DOUBLE_EQ(kilometer.convert_value_to(1, meter), 1000.0);
-  ASSERT_THROW(meter.convert_value_to(1, second), std::invalid_argument);
+  // Watt = Joule / Second
+  Unit watt = common_units::Watt;
+  Unit joule_per_second = common_units::Joule / common_units::Second;
+  ASSERT_EQ(watt.dimensions(), joule_per_second.dimensions());
+
+  // Volt = Watt / Ampere
+  Unit volt = common_units::Volt;
+  Unit watt_per_ampere = common_units::Watt / common_units::Ampere;
+  ASSERT_EQ(volt.dimensions(), watt_per_ampere.dimensions());
+
+  // Ohm = Volt / Ampere
+  Unit ohm = common_units::Ohm;
+  Unit volt_per_ampere = common_units::Volt / common_units::Ampere;
+  ASSERT_EQ(ohm.dimensions(), volt_per_ampere.dimensions());
 }
 
-TEST_F(UnitTest, OffsetConversion) {
-  Unit kelvin({{SI::DIMENSION_TEMPERATURE, 1}}, 1.0, 0.0);
-  Unit celsius({{SI::DIMENSION_TEMPERATURE, 1}}, 1.0, CELSIUS_OFFSET);
+// Non-SI units
+TEST(TestCommonUnits, NonSIUnits) {
+  // Minute
+  Unit minute = common_units::Minute;
+  TotalDimensions time_dim = {{SI::DIMENSION_TIME, 1}};
+  ASSERT_EQ(minute.dimensions(), time_dim);
+  ASSERT_DOUBLE_EQ(minute.scale_factor(), 60.0);
 
-  // 0 C is 273.15 K
-  ASSERT_NEAR(celsius.convert_value_to(0, kelvin), 273.15, 1e-9);
-  // 273.15 K is 0 C
-  ASSERT_NEAR(kelvin.convert_value_to(273.15, celsius), 0.0, 1e-9);
+  // Hour
+  Unit hour = common_units::Hour;
+  ASSERT_EQ(hour.dimensions(), time_dim);
+  ASSERT_DOUBLE_EQ(hour.scale_factor(), 3600.0);
+
+  // Electron volt
+  Unit ev = common_units::ElectronVolt;
+  TotalDimensions ev_dims = {
+      {SI::DIMENSION_MASS, 1},
+      {SI::DIMENSION_LENGTH, 2},
+      {SI::DIMENSION_TIME, -2},
+  };
+  ASSERT_EQ(ev.dimensions(), ev_dims);
+  ASSERT_NEAR(ev.scale_factor(), 1.602176634e-19, 1e-25);
 }
 
-TEST_F(UnitTest, WithPrefix) {
-  Unit meter(dim_len_);
-  auto kilometer = meter.with_prefix("k");
+// Temperature units
+TEST(TestCommonUnits, TemperatureUnits) {
+  // Kelvin (base unit)
+  Unit kelvin = common_units::Kelvin;
+  TotalDimensions temp_dim = {{SI::DIMENSION_TEMPERATURE, 1}};
+  ASSERT_EQ(kelvin.dimensions(), temp_dim);
+  ASSERT_DOUBLE_EQ(kelvin.scale_factor(), 1.0);
+  ASSERT_DOUBLE_EQ(kelvin.offset(), 0.0);
 
-  ASSERT_DOUBLE_EQ(kilometer->scale_factor(), 1000.0);
-  ASSERT_EQ(kilometer->prefix(), "k");
-  ASSERT_TRUE(kilometer->is_compatible_with(meter));
+  // Celsius
+  Unit celsius = common_units::Celsius;
+  ASSERT_EQ(celsius.dimensions(), temp_dim);
+  ASSERT_DOUBLE_EQ(celsius.scale_factor(), 1.0);
+  ASSERT_DOUBLE_EQ(celsius.offset(), 273.15);
 
-  auto millimeter = meter.with_prefix("m");
-  ASSERT_DOUBLE_EQ(millimeter->scale_factor(), 0.001);
-  ASSERT_EQ(millimeter->prefix(), "m");
+  // Fahrenheit
+  Unit fahrenheit = common_units::Fahrenheit;
+  ASSERT_EQ(fahrenheit.dimensions(), temp_dim);
+  ASSERT_NEAR(fahrenheit.scale_factor(), 5.0 / 9.0, 1e-10);
+  ASSERT_NEAR(fahrenheit.offset(), 459.67, 1e-10);
 
-  ASSERT_THROW(meter.with_prefix("invalid_prefix"), std::invalid_argument);
+  // Temperature conversion validation
+  // 0°C = 273.15K
+  ASSERT_NEAR(celsius.convert_value_to(0.0, kelvin), 273.15, 1e-10);
+  // 0K = -273.15°C
+  ASSERT_NEAR(kelvin.convert_value_to(0.0, celsius), -273.15, 1e-10);
+  // 32°F = 0°C
+  ASSERT_NEAR(fahrenheit.convert_value_to(32.0, celsius), 0.0, 1e-10);
 }
 
-TEST_F(UnitTest, Multiplication) {
-  Unit meter(dim_len_);
-  Unit second(dim_time_);
+// Dimensionless units
+TEST(TestCommonUnits, DimensionlessUnits) {
+  Unit dimensionless = common_units::Dimensionless;
+  TotalDimensions empty_dims;
+  ASSERT_EQ(dimensionless.dimensions(), empty_dims);
+  ASSERT_DOUBLE_EQ(dimensionless.scale_factor(), 1.0);
 
-  auto meter_second = meter * second;
-  auto expected_dims =
-      TotalDimensions{{SI::DIMENSION_LENGTH, 1}, {SI::DIMENSION_TIME, 1}};
-  ASSERT_EQ(meter_second->dimensions(), expected_dims);
-  ASSERT_DOUBLE_EQ(meter_second->scale_factor(), 1.0);
+  Unit percent = common_units::Percent;
+  ASSERT_EQ(percent.dimensions(), empty_dims);
+  ASSERT_DOUBLE_EQ(percent.scale_factor(), 0.01);
+
+  // 100% = 1.0 dimensionless
+  ASSERT_DOUBLE_EQ(percent.convert_value_to(100.0, dimensionless), 1.0);
 }
 
-TEST_F(UnitTest, Division) {
-  Unit meter(dim_len_);
-  Unit second(dim_time_);
-  auto m_per_s = meter / second;
+// Get unit with prefix
+TEST(TestCommonUnits, GetUnitWithPrefix) {
+  // General prefix method
+  Unit millimeter = common_units::get_unit_with_prefix(common_units::Meter, SI::MILLI_SYMBOL);
+  TotalDimensions len_dim = {{SI::DIMENSION_LENGTH, 1}};
+  ASSERT_EQ(millimeter.dimensions(), len_dim);
+  ASSERT_DOUBLE_EQ(millimeter.scale_factor(), 0.001);
 
-  auto expected_dims =
-      TotalDimensions{{SI::DIMENSION_LENGTH, 1}, {SI::DIMENSION_TIME, -1}};
-  ASSERT_EQ(m_per_s->dimensions(), expected_dims);
+  // Specific prefix methods
+  Unit km = common_units::get_kilo(common_units::Meter);
+  ASSERT_DOUBLE_EQ(km.scale_factor(), 1000.0);
+
+  Unit uA = common_units::get_micro(common_units::Ampere);
+  ASSERT_DOUBLE_EQ(uA.scale_factor(), 1e-6);
+
+  Unit mV = common_units::get_milli(common_units::Volt);
+  ASSERT_DOUBLE_EQ(mV.scale_factor(), 0.001);
+
+  Unit MW = common_units::get_mega(common_units::Watt);
+  ASSERT_DOUBLE_EQ(MW.scale_factor(), 1e6);
 }
 
-TEST_F(UnitTest, Power) {
-  Unit meter(dim_len_);
-  auto m2      = meter ^ 2;
-  auto dims_m2 = m2->dimensions();
-  ASSERT_EQ(dims_m2.size(), 1);
-  ASSERT_EQ(dims_m2.at(SI::DIMENSION_LENGTH), 2);
+// Conversion between prefixed units
+TEST(TestCommonUnits, ConversionBetweenPrefixedUnits) {
+  // 1 km to m
+  Unit km = common_units::get_kilo(common_units::Meter);
+  double meters_in_km = km.convert_value_to(1.0, common_units::Meter);
+  ASSERT_DOUBLE_EQ(meters_in_km, 1000.0);
 
-  auto inv_m      = meter ^ -1;
-  auto dims_inv_m = inv_m->dimensions();
-  ASSERT_EQ(dims_inv_m.size(), 1);
-  ASSERT_EQ(dims_inv_m.at(SI::DIMENSION_LENGTH), -1);
+  // 1000 mV to V
+  Unit mV = common_units::get_milli(common_units::Volt);
+  double volts_in_mV = mV.convert_value_to(1000.0, common_units::Volt);
+  ASSERT_DOUBLE_EQ(volts_in_mV, 1.0);
 
-  auto m0 = meter ^ 0;
-  ASSERT_TRUE(m0->dimensions().empty());
+  // 1 MW to kW
+  Unit MW = common_units::get_mega(common_units::Watt);
+  Unit kW = common_units::get_kilo(common_units::Watt);
+  double kW_in_MW = MW.convert_value_to(1.0, kW);
+  ASSERT_DOUBLE_EQ(kW_in_MW, 1000.0);
 }
