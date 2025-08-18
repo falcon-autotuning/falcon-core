4376154e160687c82622b33f919adf5bb7f3599a
jvlad1979 <daschug1@gmail.com>
2025-08-18 14:00:14 -0400

    refactor: improve formatting of CEREAL registration macros in BaseLabelledArray.cpp

--- src/math/labelled_arrays/BaseLabelledArray.cpp
+++ src/math/labelled_arrays/BaseLabelledArray.cpp
@@ -1,7 +1,12 @@
 #include "falcon_core/math/labelled_arrays/BaseLabelledArray.hpp"
+
 #include <cereal/types/polymorphic.hpp>
+
 #include "falcon_core/generic/Song.hpp"
 
 // Example registration for float/int, add more as needed
-CEREAL_REGISTER_TYPE(falcon_core::math::labelled_arrays::BaseLabelledArray<float, int>)
-CEREAL_REGISTER_POLYMORPHIC_RELATION(falcon_core::generic::Song, falcon_core::math::labelled_arrays::BaseLabelledArray<float, int>)
+CEREAL_REGISTER_TYPE(
+    falcon_core::math::labelled_arrays::BaseLabelledArray<float, int>)
+CEREAL_REGISTER_POLYMORPHIC_RELATION(
+    falcon_core::generic::Song,
+    falcon_core::math::labelled_arrays::BaseLabelledArray<float, int>)
