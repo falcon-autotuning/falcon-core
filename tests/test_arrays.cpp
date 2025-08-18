bdf3dacdb0048d9ed320f4a93d19e39b5a4c3edb
jvlad1979 <daschug1@gmail.com>
2025-08-18 16:26:35 -0400

    test: Add SerializationRoundTrip tests for BaseArray and ControlArray1D

--- tests/test_arrays.cpp
+++ tests/test_arrays.cpp
@@ -30,3 +30,58 @@ TEST(ControlArray1DTest, ThrowsOnNon1D) {
     // Use a lambda to avoid "most vexing parse" and ensure exception is thrown on construction
     EXPECT_THROW({ ControlArray1D arr(mat); }, std::invalid_argument);
 }
+
+#include <cereal/archives/json.hpp>
+#include <cereal/types/memory.hpp>
+#include <sstream>
+
+TEST(BaseArrayTest, SerializationRoundTrip) {
+    Eigen::MatrixXd mat(2, 2);
+    mat << 1.0, 2.0, 3.0, 4.0;
+    BaseArray<double> arr(mat);
+
+    // Serialize to JSON
+    std::stringstream ss;
+    {
+        cereal::JSONOutputArchive oarchive(ss);
+        oarchive(arr);
+    }
+    std::cout << "Serialized JSON:\n" << ss.str() << std::endl;
+
+    // Deserialize from JSON
+    BaseArray<double> arr2;
+    {
+        cereal::JSONInputArchive iarchive(ss);
+        iarchive(arr2);
+    }
+
+    EXPECT_EQ(arr2.data().rows(), 2);
+    EXPECT_EQ(arr2.data().cols(), 2);
+    EXPECT_DOUBLE_EQ(arr2.data()(0, 0), 1.0);
+    EXPECT_DOUBLE_EQ(arr2.data()(1, 1), 4.0);
+}
+
+TEST(ControlArray1DTest, SerializationRoundTrip) {
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> vec(1, 3);
+    vec << 1.0, 2.0, 3.0;
+    ControlArray1D arr1d(vec);
+
+    // Serialize to JSON
+    std::stringstream ss;
+    {
+        cereal::JSONOutputArchive oarchive(ss);
+        oarchive(arr1d);
+    }
+    std::cout << "Serialized JSON:\n" << ss.str() << std::endl;
+
+    // Deserialize from JSON
+    ControlArray1D arr1d2;
+    {
+        cereal::JSONInputArchive iarchive(ss);
+        iarchive(arr1d2);
+    }
+
+    EXPECT_EQ(arr1d2.data().rows(), 1);
+    EXPECT_EQ(arr1d2.data().cols(), 3);
+    EXPECT_DOUBLE_EQ(arr1d2.data()(0, 2), 3.0);
+}
