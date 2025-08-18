09e8d7d3e8eb3fd92f98316ce8abe83ee7688f75
jvlad1979 <daschug1@gmail.com>
2025-08-18 14:07:53 -0400

    fix: format CEREAL_REGISTER_POLYMORPHIC_RELATION for readability

--- src/math/labelled_arrays/BaseLabelledArray.cpp
+++ src/math/labelled_arrays/BaseLabelledArray.cpp
@@ -6,4 +6,5 @@ using namespace falcon_core;
 using namespace falcon_core::math::labelled_arrays;
 
 CEREAL_REGISTER_TYPE((BaseLabelledArray<float, int>))
-CEREAL_REGISTER_POLYMORPHIC_RELATION(generic::Song, (BaseLabelledArray<float, int>))
+CEREAL_REGISTER_POLYMORPHIC_RELATION(generic::Song,
+                                     (BaseLabelledArray<float, int>))
