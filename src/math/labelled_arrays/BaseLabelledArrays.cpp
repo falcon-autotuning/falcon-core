9e8a7479a05192ca6fb01c92e45ffe31c7479576
jvlad1979 <daschug1@gmail.com>
2025-08-18 14:34:11 -0400

    refactor: Improve formatting and organization in BaseLabelledArrays.cpp

--- src/math/labelled_arrays/BaseLabelledArrays.cpp
+++ src/math/labelled_arrays/BaseLabelledArrays.cpp
@@ -1,8 +1,14 @@
 #include "falcon_core/math/labelled_arrays/BaseLabelledArrays.hpp"
+
 #include <cereal/types/polymorphic.hpp>
+
+#include "falcon_core/math/arrays/BaseArray.hpp"
 
 using namespace falcon_core;
 using namespace falcon_core::math::labelled_arrays;
 
-CEREAL_REGISTER_TYPE(BaseLabelledArrays<falcon_core::math::arrays::BaseArray<double>>)
-CEREAL_REGISTER_POLYMORPHIC_RELATION(generic::Song, BaseLabelledArrays<falcon_core::math::arrays::BaseArray<double>>)
+CEREAL_REGISTER_TYPE(
+    BaseLabelledArrays<falcon_core::math::arrays::BaseArray<double>>)
+CEREAL_REGISTER_POLYMORPHIC_RELATION(
+    generic::Song,
+    BaseLabelledArrays<falcon_core::math::arrays::BaseArray<double>>)
