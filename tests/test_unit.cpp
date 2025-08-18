62e6f3f0a0f677dcc0785ef747f9d1802b6d7db6
jvlad1979 <daschug1@gmail.com>
2025-08-18 14:48:30 -0400

    test: add serialization round-trip test for SymbolUnit in test_unit.cpp

--- tests/test_unit.cpp
+++ tests/test_unit.cpp
@@ -163,6 +163,31 @@ TEST(TestSymbolUnit, Comparison) {
   // Symbol comparison may differ
 }
 
+#include <sstream>
+#include <cereal/archives/json.hpp>
+#include <cereal/types/memory.hpp>
+
+TEST(TestSymbolUnit, SerializationRoundTrip) {
+  SymbolUnit m = Meter();
+
+  // Serialize to JSON
+  std::stringstream ss;
+  {
+    cereal::JSONOutputArchive oarchive(ss);
+    oarchive(m);
+  }
+
+  // Deserialize from JSON
+  SymbolUnit m2;
+  {
+    cereal::JSONInputArchive iarchive(ss);
+    iarchive(m2);
+  }
+
+  ASSERT_EQ(m.symbol(), m2.symbol());
+  ASSERT_EQ(m.name(), m2.name());
+}
+
 TEST(TestSymbolUnit, StringRepresentations) {
   SymbolUnit m = Meter();
 
