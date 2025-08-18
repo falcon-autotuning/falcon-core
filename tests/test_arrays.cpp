f6cac7e426180c9742f98bce76442c3883362e2d
jvlad1979 <daschug1@gmail.com>
2025-08-18 16:48:05 -0400

    test: Update tests to use to_json_string and from_json_string methods for serialization

--- tests/test_arrays.cpp
+++ tests/test_arrays.cpp
@@ -43,46 +43,31 @@ TEST(BaseArrayTest, SerializationRoundTrip) {
   mat << 1.0, 2.0, 3.0, 4.0;
   BaseArray<double> arr(mat);
 
-  // Serialize to JSON
-  std::stringstream ss;
-  {
-    cereal::JSONOutputArchive oarchive(ss);
-    oarchive(arr);
-  }
-  std::cout << "Serialized JSON:\n" << ss.str() << std::endl;
+  // Serialize to JSON using the class helper
+  std::string json = arr.to_json_string();
+  std::cout << "Serialized JSON:\n" << json << std::endl;
 
-  // Deserialize from JSON
-  BaseArray<double> arr2;
-  {
-    cereal::JSONInputArchive iarchive(ss);
-    iarchive(arr2);
-  }
+  // Deserialize from JSON using the class helper
+  auto arr2 = BaseArray<double>::from_json_string<BaseArray<double>>(json);
 
-  EXPECT_EQ(arr2.data().rows(), 2);
-  EXPECT_EQ(arr2.data().cols(), 2);
-  EXPECT_DOUBLE_EQ(arr2.data()(0, 0), 1.0);
-  EXPECT_DOUBLE_EQ(arr2.data()(1, 1), 4.0);
+  EXPECT_EQ(arr2->data().rows(), 2);
+  EXPECT_EQ(arr2->data().cols(), 2);
+  EXPECT_DOUBLE_EQ(arr2->data()(0, 0), 1.0);
+  EXPECT_DOUBLE_EQ(arr2->data()(1, 1), 4.0);
 }
 
 TEST(ControlArray1DTest, SerializationRoundTrip) {
   Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> vec(1, 3);
   vec << 1.0, 2.0, 3.0;
-  ControlArray1D arr1d(vec);
+  auto arr1d = std::make_shared<ControlArray1D>(vec);
 
-  // Serialize to JSON
-  std::stringstream ss;
-  {
-    cereal::JSONOutputArchive oarchive(ss);
-    oarchive(arr1d);
-  }
-  std::cout << "Serialized JSON:\n" << ss.str() << std::endl;
+  // Serialize to JSON using the class helper
+  std::string json = arr1d->to_json_string();
+  std::cout << "Serialized JSON:\n" << json << std::endl;
 
-  // Deserialize from JSON
-  std::shared_ptr<ControlArray1D> arr1d2;
-  {
-    cereal::JSONInputArchive iarchive(ss);
-    iarchive(arr1d2);
-  }
+  // Deserialize from JSON using the class helper
+  auto arr1d2 = ControlArray1D::from_json_string<ControlArray1D>(json);
+
   EXPECT_EQ(arr1d2->data().rows(), 1);
   EXPECT_EQ(arr1d2->data().cols(), 3);
   EXPECT_DOUBLE_EQ(arr1d2->data()(0, 2), 3.0);
