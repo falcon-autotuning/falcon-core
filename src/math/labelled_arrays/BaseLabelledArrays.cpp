350a75c0c920e492d509ae857525c261aa87bb47
jvlad1979 <daschug1@gmail.com>
2025-08-18 14:39:01 -0400

    fix: use type aliases to resolve Cereal macro parsing issues

--- src/math/labelled_arrays/BaseLabelledArrays.cpp
+++ src/math/labelled_arrays/BaseLabelledArrays.cpp
@@ -1,17 +1,12 @@
 #include "falcon_core/math/labelled_arrays/BaseLabelledArrays.hpp"
 
 #include <cereal/types/polymorphic.hpp>
-
 #include "falcon_core/math/arrays/BaseArray.hpp"
 #include "falcon_core/math/labelled_arrays/BaseLabelledArray.hpp"
 
-using namespace falcon_core;
-using namespace falcon_core::math::labelled_arrays;
+// Type aliases to avoid macro issues with template commas
+using LabelledArrayType = falcon_core::math::labelled_arrays::BaseLabelledArray<falcon_core::math::arrays::BaseArray<double>, int>;
+using LabelledArraysType = falcon_core::math::labelled_arrays::BaseLabelledArrays<LabelledArrayType>;
 
-CEREAL_REGISTER_TYPE(
-    (BaseLabelledArrays<
-        BaseLabelledArray<falcon_core::math::arrays::BaseArray<double>, int>>))
-CEREAL_REGISTER_POLYMORPHIC_RELATION(
-    generic::Song,
-    (BaseLabelledArrays<
-        BaseLabelledArray<falcon_core::math::arrays::BaseArray<double>, int>>))
+CEREAL_REGISTER_TYPE(LabelledArraysType)
+CEREAL_REGISTER_POLYMORPHIC_RELATION(falcon_core::generic::Song, LabelledArraysType)
