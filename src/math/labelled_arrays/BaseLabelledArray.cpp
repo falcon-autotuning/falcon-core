152e7b79111ad2bc8a61fe17adc32518f6602b9a
jvlad1979 <daschug1@gmail.com>
2025-08-18 14:42:30 -0400

    refactor: simplify code by introducing alias for BaseLabelledArray

--- src/math/labelled_arrays/BaseLabelledArray.cpp
+++ src/math/labelled_arrays/BaseLabelledArray.cpp
@@ -6,4 +6,6 @@ using namespace falcon_core;
 using namespace falcon_core::math::labelled_arrays;
 
 CEREAL_REGISTER_TYPE(BaseLabelledArray<float, int>)
-CEREAL_REGISTER_POLYMORPHIC_RELATION(generic::Song, BaseLabelledArray<float, int>)
+
+using BLA = BaseLabelledArray<float, int>;
+CEREAL_REGISTER_POLYMORPHIC_RELATION(generic::Song, BLA)
