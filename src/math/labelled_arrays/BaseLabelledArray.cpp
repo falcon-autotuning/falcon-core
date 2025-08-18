db45cdbc23ab744f13ed4c42f508bc0f4c07e811
jvlad1979 <daschug1@gmail.com>
2025-08-18 14:07:38 -0400

    style: reorder include statements and format CEREAL registration call

--- src/math/labelled_arrays/BaseLabelledArray.cpp
+++ src/math/labelled_arrays/BaseLabelledArray.cpp
@@ -1,9 +1,10 @@
 #include "falcon_core/math/labelled_arrays/BaseLabelledArray.hpp"
 
+#include "cereal/types/polymorphic.hpp"
+
 using namespace falcon_core;
 using namespace falcon_core::math::labelled_arrays;
 
-#include <cereal/types/polymorphic.hpp>
-
 CEREAL_REGISTER_TYPE(BaseLabelledArray<float, int>)
-CEREAL_REGISTER_POLYMORPHIC_RELATION(generic::Song, BaseLabelledArray<float, int>)
+CEREAL_REGISTER_POLYMORPHIC_RELATION(generic::Song,
+                                     BaseLabelledArray<float, int>)
