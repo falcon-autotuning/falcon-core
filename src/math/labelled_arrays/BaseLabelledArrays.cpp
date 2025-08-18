10f7177dad12b370c64272aa77de31fb1ad44632
jvlad1979 <daschug1@gmail.com>
2025-08-18 14:39:00 -0400

    style: reformat CEREAL_REGISTER_TYPE and CEREAL_REGISTER_POLYMORPHIC_RELATION lines

--- src/math/labelled_arrays/BaseLabelledArrays.cpp
+++ src/math/labelled_arrays/BaseLabelledArrays.cpp
@@ -8,5 +8,10 @@
 using namespace falcon_core;
 using namespace falcon_core::math::labelled_arrays;
 
-CEREAL_REGISTER_TYPE((BaseLabelledArrays<BaseLabelledArray<falcon_core::math::arrays::BaseArray<double>, int>>))
-CEREAL_REGISTER_POLYMORPHIC_RELATION(generic::Song, (BaseLabelledArrays<BaseLabelledArray<falcon_core::math::arrays::BaseArray<double>, int>>))
+CEREAL_REGISTER_TYPE(
+    (BaseLabelledArrays<
+        BaseLabelledArray<falcon_core::math::arrays::BaseArray<double>, int>>))
+CEREAL_REGISTER_POLYMORPHIC_RELATION(
+    generic::Song,
+    (BaseLabelledArrays<
+        BaseLabelledArray<falcon_core::math::arrays::BaseArray<double>, int>>))
