fc8238df3d936ebea164dc8104d412747c4b6454
jvlad1979 <daschug1@gmail.com>
2025-08-18 14:37:00 -0400

    refactor: reorganize includes and simplify CEREAL registration calls

--- src/math/labelled_arrays/BaseLabelledArrays.cpp
+++ src/math/labelled_arrays/BaseLabelledArrays.cpp
@@ -1,14 +1,18 @@
 #include "falcon_core/math/labelled_arrays/BaseLabelledArrays.hpp"
 
 #include <cereal/types/polymorphic.hpp>
-#include "falcon_core/math/labelled_arrays/BaseLabelledArray.hpp"
+
 #include "falcon_core/math/arrays/BaseArray.hpp"
+#include "falcon_core/math/labelled_arrays/BaseLabelledArray.hpp"
 
 using namespace falcon_core;
 using namespace falcon_core::math::labelled_arrays;
 
 // Register BaseLabelledArrays<BaseLabelledArray<BaseArray<double>, int>>
-CEREAL_REGISTER_TYPE(BaseLabelledArrays<BaseLabelledArray<falcon_core::math::arrays::BaseArray<double>, int>>)
-CEREAL_REGISTER_POLYMORPHIC_RELATION(
-    generic::Song,
-    BaseLabelledArrays<BaseLabelledArray<falcon_core::math::arrays::BaseArray<double>, int>>)
+CEREAL_REGISTER_TYPE(
+    BaseLabelledArrays<
+        BaseLabelledArray<falcon_core::math::arrays::BaseArray<double>, int>>)
+
+using BLA = BaseLabelledArrays<
+    BaseLabelledArray<falcon_core::math::arrays::BaseArray<double>, int>>;
+CEREAL_REGISTER_POLYMORPHIC_RELATION(generic::Song, BLA)
