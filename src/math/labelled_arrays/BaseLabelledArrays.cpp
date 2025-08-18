8c850e8877c34df2d7ff9fccf04705714aac40bd
jvlad1979 <daschug1@gmail.com>
2025-08-18 14:34:11 -0400

    fix: correct value type for BaseLabelledArrays to use BaseLabelledArray

--- src/math/labelled_arrays/BaseLabelledArrays.cpp
+++ src/math/labelled_arrays/BaseLabelledArrays.cpp
@@ -1,14 +1,14 @@
 #include "falcon_core/math/labelled_arrays/BaseLabelledArrays.hpp"
 
 #include <cereal/types/polymorphic.hpp>
-
+#include "falcon_core/math/labelled_arrays/BaseLabelledArray.hpp"
 #include "falcon_core/math/arrays/BaseArray.hpp"
 
 using namespace falcon_core;
 using namespace falcon_core::math::labelled_arrays;
 
-CEREAL_REGISTER_TYPE(
-    BaseLabelledArrays<falcon_core::math::arrays::BaseArray<double>>)
+// Register BaseLabelledArrays<BaseLabelledArray<BaseArray<double>, int>>
+CEREAL_REGISTER_TYPE(BaseLabelledArrays<BaseLabelledArray<falcon_core::math::arrays::BaseArray<double>, int>>)
 CEREAL_REGISTER_POLYMORPHIC_RELATION(
     generic::Song,
-    BaseLabelledArrays<falcon_core::math::arrays::BaseArray<double>>)
+    BaseLabelledArrays<BaseLabelledArray<falcon_core::math::arrays::BaseArray<double>, int>>)
--- tests/test_labelled_arrays.cpp
+++ tests/test_labelled_arrays.cpp
@@ -4,6 +4,7 @@
 
 #include "falcon_core/math/arrays/BaseArray.hpp"
 #include "falcon_core/math/labelled_arrays/BaseLabelledArrays.hpp"
+#include "falcon_core/math/labelled_arrays/BaseLabelledArray.hpp"
 #include "falcon_core/math/labelled_arrays/IsLabelled1D.hpp"
 
 using namespace falcon_core::math::labelled_arrays;
@@ -19,21 +20,30 @@ class DummyLabelled1D : public IsLabelled1D<DummyLabelled1D> {
   BaseArray<double> _array;
 };
 
+using ArrayType = BaseArray<double>;
+using LabelType = int;
+using LabelledArrayType = BaseLabelledArray<ArrayType, LabelType>;
+using LabelledArraysType = BaseLabelledArrays<LabelledArrayType>;
+
 TEST(BaseLabelledArraysTest, ConstructionAndAccess) {
   Eigen::MatrixXd mat1(1, 3);
   mat1 << 1.0, 2.0, 3.0;
   Eigen::MatrixXd mat2(1, 3);
   mat2 << 4.0, 5.0, 6.0;
-  auto arr1 = std::make_shared<BaseArray<double>>(mat1);
-  auto arr2 = std::make_shared<BaseArray<double>>(mat2);
+  auto arr1 = std::make_shared<ArrayType>(mat1);
+  auto arr2 = std::make_shared<ArrayType>(mat2);
+  auto labelled1 = std::make_shared<LabelledArrayType>(arr1, std::make_shared<LabelType>(42));
+  auto labelled2 = std::make_shared<LabelledArrayType>(arr2, std::make_shared<LabelType>(43));
 
-  BaseLabelledArrays<BaseArray<double>> labelled_arrays;
-  labelled_arrays.append(arr1);
-  labelled_arrays.append(arr2);
+  LabelledArraysType labelled_arrays;
+  labelled_arrays.append(labelled1);
+  labelled_arrays.append(labelled2);
 
   EXPECT_EQ(labelled_arrays.get_arrays().size(), 2);
-  EXPECT_DOUBLE_EQ(labelled_arrays.get_arrays()[0]->data()(0, 1), 2.0);
-  EXPECT_DOUBLE_EQ(labelled_arrays.get_arrays()[1]->data()(0, 2), 6.0);
+  EXPECT_TRUE(labelled_arrays.get_arrays()[0]->array()->data().isApprox(mat1));
+  EXPECT_TRUE(labelled_arrays.get_arrays()[1]->array()->data().isApprox(mat2));
+  EXPECT_EQ(*labelled_arrays.get_arrays()[0]->label(), 42);
+  EXPECT_EQ(*labelled_arrays.get_arrays()[1]->label(), 43);
 }
 
 TEST(BaseLabelledArraysTest, SerializationRoundTrip) {
@@ -41,22 +51,26 @@ TEST(BaseLabelledArraysTest, SerializationRoundTrip) {
   mat1 << 1.0, 2.0, 3.0;
   Eigen::MatrixXd mat2(1, 3);
   mat2 << 4.0, 5.0, 6.0;
-  auto arr1 = std::make_shared<BaseArray<double>>(mat1);
-  auto arr2 = std::make_shared<BaseArray<double>>(mat2);
+  auto arr1 = std::make_shared<ArrayType>(mat1);
+  auto arr2 = std::make_shared<ArrayType>(mat2);
+  auto labelled1 = std::make_shared<LabelledArrayType>(arr1, std::make_shared<LabelType>(42));
+  auto labelled2 = std::make_shared<LabelledArrayType>(arr2, std::make_shared<LabelType>(43));
 
-  BaseLabelledArrays<BaseArray<double>> labelled_arrays;
-  labelled_arrays.append(arr1);
-  labelled_arrays.append(arr2);
+  LabelledArraysType labelled_arrays;
+  labelled_arrays.append(labelled1);
+  labelled_arrays.append(labelled2);
 
   // Serialize to JSON string
   std::string json = labelled_arrays.to_json_string();
 
   // Deserialize from JSON string
-  auto deserialized = falcon_core::generic::Song::from_json_string<BaseLabelledArrays<BaseArray<double>>>(json);
+  auto deserialized = falcon_core::generic::Song::from_json_string<LabelledArraysType>(json);
 
   ASSERT_EQ(deserialized->get_arrays().size(), 2);
-  EXPECT_TRUE(deserialized->get_arrays()[0]->data().isApprox(mat1));
-  EXPECT_TRUE(deserialized->get_arrays()[1]->data().isApprox(mat2));
+  EXPECT_TRUE(deserialized->get_arrays()[0]->array()->data().isApprox(mat1));
+  EXPECT_TRUE(deserialized->get_arrays()[1]->array()->data().isApprox(mat2));
+  EXPECT_EQ(*deserialized->get_arrays()[0]->label(), 42);
+  EXPECT_EQ(*deserialized->get_arrays()[1]->label(), 43);
 }
 
 TEST(IsLabelled1DTest, GetStart) {
