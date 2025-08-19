b3fbde775b7580964175e6337e59d1a91e2e6130
jvlad1979 <daschug1@gmail.com>
2025-08-19 12:37:57 -0400

    refactor: replace Axes vector inheritance with private _items vector

--- include/falcon_core/math/Axes.hpp
+++ include/falcon_core/math/Axes.hpp
@@ -1,6 +1,9 @@
 #pragma once
-#include <cereal/types/map.hpp>
+#include <cereal/types/vector.hpp>
 #include <cereal/types/memory.hpp>
+#include <vector>
+#include <memory>
+#include <stdexcept>
 
 #include "falcon_core/generic/Song.hpp"
 
@@ -8,20 +11,30 @@ namespace falcon_core {
 namespace math {
 
 template <typename T>
-class Axes : public std::vector<std::shared_ptr<T>>, public generic::Song {
+class Axes : public generic::Song {
  public:
-  using std::vector<std::shared_ptr<T>>::vector;
+  Axes() = default;
+  explicit Axes(const std::vector<std::shared_ptr<T>>& items) : _items(items) {}
+
+  // Vector-like methods
+  size_t size() const { return _items.size(); }
+  bool empty() const { return _items.empty(); }
+  void push_back(const std::shared_ptr<T>& item) { _items.push_back(item); }
+  const std::shared_ptr<T>& at(size_t idx) const { return _items.at(idx); }
+  std::shared_ptr<T>& at(size_t idx) { return _items.at(idx); }
+  const std::vector<std::shared_ptr<T>>& items() const { return _items; }
+  std::vector<std::shared_ptr<T>>& items() { return _items; }
+  const std::shared_ptr<T>& operator[](size_t idx) const { return _items[idx]; }
+  std::shared_ptr<T>& operator[](size_t idx) { return _items[idx]; }
 
  private:
+  std::vector<std::shared_ptr<T>> _items;
+
+  friend class cereal::access;
   template <class Archive>
   void serialize(Archive& ar) {
-    ar(cereal::base_class<generic::Song>(this),
-       cereal::base_class<std::vector<std::shared_ptr<T>>>(this));
+    ar(cereal::base_class<generic::Song>(this), _items);
   }
-
- protected:
-  Axes() = default;
-  friend class cereal::access;
 };
 
 using namespace falcon_core::math;
