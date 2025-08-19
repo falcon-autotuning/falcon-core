726b3098f0bdc899eae0a26cbe88e6def90d4309
jvlad1979 <daschug1@gmail.com>
2025-08-19 05:15:38 -0400

    fix:: misc improvements to testing

--- src/math/arrays/ControlArray1D.cpp
+++ src/math/arrays/ControlArray1D.cpp
@@ -6,4 +6,4 @@ using namespace falcon_core::math::arrays;
 
 // Register ControlArray1D for polymorphic serialization
 CEREAL_REGISTER_TYPE(ControlArray1D)
-CEREAL_REGISTER_POLYMORPHIC_RELATION(generic::Song, ControlArray1D)
+CEREAL_REGISTER_POLYMORPHIC_RELATION(falcon_core::generic::Song, ControlArray1D)
