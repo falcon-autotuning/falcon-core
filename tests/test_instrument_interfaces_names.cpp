89f6ad95af22f555c30a103ce1ae6775c51f3e34
jvlad1979 <daschug1@gmail.com>
2025-08-20 12:57:04 -0400

    feature: fixed ports, knobs and meters to use proper vector wrapping

--- tests/test_instrument_interfaces_names.cpp
+++ tests/test_instrument_interfaces_names.cpp
@@ -1,3 +1,5 @@
 #include "falcon_core/instrument_interfaces/names/InstrumentPort.hpp"
 #include "falcon_core/instrument_interfaces/names/Knob.hpp"
+#include "falcon_core/instrument_interfaces/names/Knobs.hpp"
 #include "falcon_core/instrument_interfaces/names/Meter.hpp"
+#include "falcon_core/instrument_interfaces/names/Ports.hpp""
