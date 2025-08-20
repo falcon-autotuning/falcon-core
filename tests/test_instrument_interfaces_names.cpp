3c65579fcd4716cf566aae5f6b15d274251c020e
jvlad1979 <daschug1@gmail.com>
2025-08-20 12:48:07 -0400

    chore: forgot to add test for instrument interfaces names

---
+++ tests/test_instrument_interfaces_names.cpp
@@ -0,0 +1,3 @@
+#include "falcon_core/instrument_interfaces/names/InstrumentPort.hpp"
+#include "falcon_core/instrument_interfaces/names/Knob.hpp"
+#include "falcon_core/instrument_interfaces/names/Meter.hpp"
