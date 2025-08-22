06cf9319876095c065f125144b07432b766e5656
Daniel Schug <daschug1@gmail.com>
2025-08-22 12:31:28 -0400

    feat: port PolarDiscretizer and ensure cereal registration for all discretizers

--- include/falcon_core/math/discretizers/PolarDiscretizer.hpp
+++ include/falcon_core/math/discretizers/PolarDiscretizer.hpp
@@ -1,23 +1,30 @@
 #pragma once
 
 #include "falcon_core/math/discretizers/BaseDiscretizer.hpp"
+#include "falcon_core/Constants.hpp"
+#include "falcon_core/math/domains/Domain.hpp"
 
 namespace falcon_core {
 namespace math {
 namespace discretizers {
 
-class CartesianDiscretizer : public BaseDiscretizer {
+class PolarDiscretizer : public BaseDiscretizer {
  public:
-  using BaseDiscretizer::BaseDiscretizer;
+  PolarDiscretizer(double delta)
+      : BaseDiscretizer(
+            delta,
+            std::make_shared<domains::Domain>(
+                -2.0 * falcon_core::PI, 2.0 * falcon_core::PI)) {}
 
  private:
   friend class cereal::access;
-  CartesianDiscretizer() = default;
+  PolarDiscretizer() = default;
   template <class Archive>
   void serialize(Archive& ar) {
     ar(cereal::base_class<BaseDiscretizer>(this));
   }
 };
+
 }  // namespace discretizers
 }  // namespace math
 }  // namespace falcon_core
@@ -25,8 +32,8 @@ class CartesianDiscretizer : public BaseDiscretizer {
 using namespace falcon_core::math::discretizers;
 
 #ifndef SWIG
-CEREAL_REGISTER_TYPE(falcon_core::math::discretizers::CartesianDiscretizer)
+CEREAL_REGISTER_TYPE(falcon_core::math::discretizers::PolarDiscretizer)
 CEREAL_REGISTER_POLYMORPHIC_RELATION(
     falcon_core::generic::Song,
-    falcon_core::math::discretizers::CartesianDiscretizer)
+    falcon_core::math::discretizers::PolarDiscretizer)
 #endif
