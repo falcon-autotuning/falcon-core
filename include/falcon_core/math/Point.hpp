cc9066546d97a70320fe38647801eb2b460c7e5c
Daniel Schug <daschug1@gmail.com>
2025-08-25 15:52:04 -0400

    fix: correct formatting of polymorphic relation registration in Point.hpp

--- include/falcon_core/math/Point.hpp
+++ include/falcon_core/math/Point.hpp
@@ -126,6 +126,5 @@ class Point : public generic::Song {
 #ifndef SWIG
 using namespace falcon_core::math;
 CEREAL_REGISTER_TYPE(falcon_core::math::Point)
-CEREAL_REGISTER_POLYMORPHIC_RELATION(falcon_core::generic::Song,
-                                     falcon_core::math::Point)
+CEREAL_REGISTER_POLYMORPHIC_RELATION(falcon_core::generic::Song, falcon_core::math::Point)
 #endif
