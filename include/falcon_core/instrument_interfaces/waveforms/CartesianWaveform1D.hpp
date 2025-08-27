953f4c7516127adf16009c440a4ddd44cc46ebb9
Daniel Schug <daschug1@gmail.com>
2025-08-27 12:27:22 -0400

    style: Improve code formatting and adjust multiline comments in waveforms

--- include/falcon_core/instrument_interfaces/waveforms/CartesianWaveform1D.hpp
+++ include/falcon_core/instrument_interfaces/waveforms/CartesianWaveform1D.hpp
@@ -1,23 +1,30 @@
 #pragma once
 
 #include <memory>
+
 #include "falcon_core/instrument_interfaces/waveforms/BaseWaveform.hpp"
 #include "falcon_core/math/discrete_spaces/CartesianDiscreteSpace1D.hpp"
 
 /**
- * @brief A 1D measurement on a cartesian domain with any time-dependent phenomena.
+ * @brief A 1D measurement on a cartesian domain with any time-dependent
+ * phenomena.
  */
 namespace falcon_core {
 namespace instrument_interfaces {
 namespace waveforms {
 
-class CartesianWaveform1D : public BaseWaveform<falcon_core::math::discrete_spaces::CartesianDiscreteSpace1D> {
+class CartesianWaveform1D
+    : public BaseWaveform<
+          falcon_core::math::discrete_spaces::CartesianDiscreteSpace1D> {
  public:
-  using space_type = falcon_core::math::discrete_spaces::CartesianDiscreteSpace1D;
+  using space_type =
+      falcon_core::math::discrete_spaces::CartesianDiscreteSpace1D;
   using base_type = BaseWaveform<space_type>;
 
-  CartesianWaveform1D(std::shared_ptr<space_type> space,
-                      std::vector<std::shared_ptr<port_transforms::PortTransform>> transforms = {})
+  CartesianWaveform1D(
+      std::shared_ptr<space_type>                                  space,
+      std::vector<std::shared_ptr<port_transforms::PortTransform>> transforms =
+          {})
       : base_type(space, transforms) {}
 
   CartesianWaveform1D() : base_type() {}
@@ -26,12 +33,16 @@ class CartesianWaveform1D : public BaseWaveform<falcon_core::math::discrete_spac
    * @brief Create a CartesianWaveform1D from raw division.
    */
   static std::shared_ptr<CartesianWaveform1D> from_division(
-      int division,
+      int                                                 division,
       std::shared_ptr<falcon_core::math::domains::Domain> shared_domain,
-      std::shared_ptr<falcon_core::math::Axes<std::map<std::string, bool>>> increasing,
-      std::vector<std::shared_ptr<port_transforms::PortTransform>> transforms = {},
-      std::shared_ptr<falcon_core::math::domains::Domain> domain = std::make_shared<falcon_core::math::domains::Domain>(0, 1)) {
-    auto space = space_type::from_division(division, shared_domain, increasing, domain);
+      std::shared_ptr<falcon_core::math::Axes<std::map<std::string, bool>>>
+                                                                   increasing,
+      std::vector<std::shared_ptr<port_transforms::PortTransform>> transforms =
+          {},
+      std::shared_ptr<falcon_core::math::domains::Domain> domain =
+          std::make_shared<falcon_core::math::domains::Domain>(0, 1)) {
+    auto space =
+        space_type::from_division(division, shared_domain, increasing, domain);
     return std::make_shared<CartesianWaveform1D>(space, transforms);
   }
 
@@ -46,6 +57,9 @@ class CartesianWaveform1D : public BaseWaveform<falcon_core::math::discrete_spac
 }  // namespace falcon_core
 
 #ifndef SWIG
-CEREAL_REGISTER_TYPE(falcon_core::instrument_interfaces::waveforms::CartesianWaveform1D)
-CEREAL_REGISTER_POLYMORPHIC_RELATION(falcon_core::generic::Song, falcon_core::instrument_interfaces::waveforms::CartesianWaveform1D)
+CEREAL_REGISTER_TYPE(
+    falcon_core::instrument_interfaces::waveforms::CartesianWaveform1D)
+CEREAL_REGISTER_POLYMORPHIC_RELATION(
+    falcon_core::generic::Song,
+    falcon_core::instrument_interfaces::waveforms::CartesianWaveform1D)
 #endif
--- include/falcon_core/instrument_interfaces/waveforms/CartesianWaveform2D.hpp
+++ include/falcon_core/instrument_interfaces/waveforms/CartesianWaveform2D.hpp
@@ -1,23 +1,29 @@
 #pragma once
 
 #include <memory>
+
 #include "falcon_core/instrument_interfaces/waveforms/BaseWaveform.hpp"
 #include "falcon_core/math/discrete_spaces/CartesianDiscreteSpace.hpp"
 
 /**
- * @brief A 2D measurement on a cartesian domain with any time-dependent phenomena.
+ * @brief A 2D measurement on a cartesian domain with any time-dependent
+ * phenomena.
  */
 namespace falcon_core {
 namespace instrument_interfaces {
 namespace waveforms {
 
-class CartesianWaveform2D : public BaseWaveform<falcon_core::math::discrete_spaces::CartesianDiscreteSpace> {
+class CartesianWaveform2D
+    : public BaseWaveform<
+          falcon_core::math::discrete_spaces::CartesianDiscreteSpace> {
  public:
   using space_type = falcon_core::math::discrete_spaces::CartesianDiscreteSpace;
-  using base_type = BaseWaveform<space_type>;
+  using base_type  = BaseWaveform<space_type>;
 
-  CartesianWaveform2D(std::shared_ptr<space_type> space,
-                      std::vector<std::shared_ptr<port_transforms::PortTransform>> transforms = {})
+  CartesianWaveform2D(
+      std::shared_ptr<space_type>                                  space,
+      std::vector<std::shared_ptr<port_transforms::PortTransform>> transforms =
+          {})
       : base_type(space, transforms) {}
 
   CartesianWaveform2D() : base_type() {}
@@ -26,12 +32,17 @@ class CartesianWaveform2D : public BaseWaveform<falcon_core::math::discrete_spac
    * @brief Create a CartesianWaveform2D from raw divisions.
    */
   static std::shared_ptr<CartesianWaveform2D> from_divisions(
-      std::shared_ptr<falcon_core::math::Axes<int>> divisions,
-      std::shared_ptr<falcon_core::math::Axes<std::shared_ptr<falcon_core::math::domains::Domain>>> axes,
-      std::shared_ptr<falcon_core::math::Axes<std::map<std::string, bool>>> increasing,
-      std::vector<std::shared_ptr<port_transforms::PortTransform>> transforms = {},
-      std::shared_ptr<falcon_core::math::domains::Domain> domain = std::make_shared<falcon_core::math::domains::Domain>(0, 1)) {
-    auto space = space_type::from_divisions(divisions, axes, domain, increasing);
+      std::shared_ptr<falcon_core::math::Axes<int>>             divisions,
+      std::shared_ptr<falcon_core::math::Axes<
+          std::shared_ptr<falcon_core::math::domains::Domain>>> axes,
+      std::shared_ptr<falcon_core::math::Axes<std::map<std::string, bool>>>
+                                                                   increasing,
+      std::vector<std::shared_ptr<port_transforms::PortTransform>> transforms =
+          {},
+      std::shared_ptr<falcon_core::math::domains::Domain> domain =
+          std::make_shared<falcon_core::math::domains::Domain>(0, 1)) {
+    auto space =
+        space_type::from_divisions(divisions, axes, domain, increasing);
     return std::make_shared<CartesianWaveform2D>(space, transforms);
   }
 
@@ -46,6 +57,9 @@ class CartesianWaveform2D : public BaseWaveform<falcon_core::math::discrete_spac
 }  // namespace falcon_core
 
 #ifndef SWIG
-CEREAL_REGISTER_TYPE(falcon_core::instrument_interfaces::waveforms::CartesianWaveform2D)
-CEREAL_REGISTER_POLYMORPHIC_RELATION(falcon_core::generic::Song, falcon_core::instrument_interfaces::waveforms::CartesianWaveform2D)
+CEREAL_REGISTER_TYPE(
+    falcon_core::instrument_interfaces::waveforms::CartesianWaveform2D)
+CEREAL_REGISTER_POLYMORPHIC_RELATION(
+    falcon_core::generic::Song,
+    falcon_core::instrument_interfaces::waveforms::CartesianWaveform2D)
 #endif
