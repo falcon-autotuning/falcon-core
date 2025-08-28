db2d482031de346f39133f2556e164d381cb5d20
Daniel Schug <daschug1@gmail.com>
2025-08-28 18:54:59 -0400

    refactor: separate declaration and implementation for AnalyticFunction class

---
+++ src/math/analytic_functions/AnalyticFunction.cpp
@@ -0,0 +1,23 @@
+#include "falcon_core/math/analytic_functions/AnalyticFunction.hpp"
+
+namespace falcon_core {
+namespace math {
+namespace analytic_functions {
+
+AnalyticFunction::AnalyticFunction() = default;
+AnalyticFunction::AnalyticFunction(const std::string& name) : _name(name) {}
+
+std::string AnalyticFunction::name() const { return _name; }
+void AnalyticFunction::set_name(const std::string& name) { _name = name; }
+
+template <class Archive>
+void AnalyticFunction::serialize(Archive& ar) {
+  ar(cereal::base_class<generic::Song>(this), _name);
+}
+
+}}} // namespace
+
+CEREAL_REGISTER_TYPE(falcon_core::math::analytic_functions::AnalyticFunction)
+CEREAL_REGISTER_POLYMORPHIC_RELATION(
+    falcon_core::generic::Song,
+    falcon_core::math::analytic_functions::AnalyticFunction)
