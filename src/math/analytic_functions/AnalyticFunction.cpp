f6fd27a8354df3c3e2deb241fc558aa80e346d94
Daniel Schug <daschug1@gmail.com>
2025-08-28 18:58:56 -0400

    fix: add explicit template instantiation for AnalyticFunction serialization

--- src/math/analytic_functions/AnalyticFunction.cpp
+++ src/math/analytic_functions/AnalyticFunction.cpp
@@ -23,3 +23,7 @@ CEREAL_REGISTER_TYPE(falcon_core::math::analytic_functions::AnalyticFunction)
 CEREAL_REGISTER_POLYMORPHIC_RELATION(
     falcon_core::generic::Song,
     falcon_core::math::analytic_functions::AnalyticFunction)
+
+// Explicit instantiation of serialize for AnalyticFunction
+template void falcon_core::math::analytic_functions::AnalyticFunction::serialize<cereal::JSONInputArchive>(cereal::JSONInputArchive&);
+template void falcon_core::math::analytic_functions::AnalyticFunction::serialize<cereal::JSONOutputArchive>(cereal::JSONOutputArchive&);
