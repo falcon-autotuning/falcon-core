be9f1768d1bf18a73440e45f939c636f2b4577ca
Daniel Schug <daschug1@gmail.com>
2025-09-03 22:11:22 -0400

    feat: port DeviceVoltageState and DeviceVoltageStates to C++

---
+++ include/falcon_core/communications/voltage_states/DeviceVoltageState.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <memory>
+#include <falcon_core/generic/Song.hpp>
+#include <falcon_core/physics/device_structures/BaseConnection.hpp>
+#include <falcon_core/physics/units/SymbolUnit.hpp>
+#include <falcon_core/math/Quantity.hpp>
+
+namespace falcon_core {
+namespace communications {
+namespace voltage_states {
+
+class DeviceVoltageState : public math::Quantity, public generic::Song {
+ public:
+  DeviceVoltageState(
+      std::shared_ptr<physics::device_structures::BaseConnection> connection,
+      double voltage,
+      std::shared_ptr<physics::units::SymbolUnit> unit);
+
+  DeviceVoltageState();
+
+  std::shared_ptr<physics::device_structures::BaseConnection> connection() const;
+  double voltage() const;
+
+ private:
+  std::shared_ptr<physics::device_structures::BaseConnection> _connection;
+
+  friend class cereal::access;
+  template <class Archive>
+  void serialize(Archive& ar);
+};
+
+}  // namespace voltage_states
+}  // namespace communications
+}  // namespace falcon_core
---
+++ src/communications/voltage_states/DeviceVoltageState.cpp
@@ -0,0 +1,38 @@
+#include <falcon_core/communications/voltage_states/DeviceVoltageState.hpp>
+
+namespace falcon_core {
+namespace communications {
+namespace voltage_states {
+
+DeviceVoltageState::DeviceVoltageState(
+    std::shared_ptr<physics::device_structures::BaseConnection> connection,
+    double voltage,
+    std::shared_ptr<physics::units::SymbolUnit> unit)
+    : math::Quantity(voltage, unit), _connection(std::move(connection)) {}
+
+DeviceVoltageState::DeviceVoltageState() = default;
+
+std::shared_ptr<physics::device_structures::BaseConnection> DeviceVoltageState::connection() const {
+  return _connection;
+}
+
+double DeviceVoltageState::voltage() const {
+  return value();
+}
+
+template <class Archive>
+void DeviceVoltageState::serialize(Archive& ar) {
+  ar(cereal::base_class<math::Quantity>(this),
+     cereal::base_class<generic::Song>(this),
+     _connection);
+}
+
+// Explicit template instantiation for cereal
+template void DeviceVoltageState::serialize<cereal::JSONOutputArchive>(cereal::JSONOutputArchive& ar);
+template void DeviceVoltageState::serialize<cereal::JSONInputArchive>(cereal::JSONInputArchive& ar);
+
+}  // namespace voltage_states
+}  // namespace communications
+}  // namespace falcon_core
+
+CEREAL_REGISTER_TYPE(falcon_core::communications::voltage_states::DeviceVoltageState)
