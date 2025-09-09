eec449baebfaa870c01d758bf94edb08fc7ef85e
Daniel Schug <daschug1@gmail.com>
2025-09-09 19:16:09 -0400

    test: extend tests for basic operations in BaseArray and ControlArray1D

--- tests/test_arrays.cpp
+++ tests/test_arrays.cpp
@@ -18,6 +18,92 @@ TEST(BaseArrayTest, ConstructionAndAccess) {
   EXPECT_DOUBLE_EQ(arr(1, 1), 4.0);
 }
 
+TEST(BaseArrayTest, BasicOperations) {
+  xt::xarray<double> arr1_data = {{1.0, 2.0}, {3.0, 4.0}};
+  xt::xarray<double> arr2_data = {{5.0, 6.0}, {7.0, 8.0}};
+  BaseArray<double> arr1(arr1_data);
+  BaseArray<double> arr2(arr2_data);
+
+  // Test operator+=
+  BaseArray<double> arr_sum = arr1;
+  arr_sum += arr2;
+  EXPECT_DOUBLE_EQ(arr_sum(0, 0), 6.0);
+  EXPECT_DOUBLE_EQ(arr_sum(0, 1), 8.0);
+  EXPECT_DOUBLE_EQ(arr_sum(1, 0), 10.0);
+  EXPECT_DOUBLE_EQ(arr_sum(1, 1), 12.0);
+
+  // Test operator-=
+  BaseArray<double> arr_diff = arr1;
+  arr_diff -= arr2;
+  EXPECT_DOUBLE_EQ(arr_diff(0, 0), -4.0);
+  EXPECT_DOUBLE_EQ(arr_diff(0, 1), -4.0);
+  EXPECT_DOUBLE_EQ(arr_diff(1, 0), -4.0);
+  EXPECT_DOUBLE_EQ(arr_diff(1, 1), -4.0);
+
+  // Test operator*=
+  BaseArray<double> arr_prod = arr1;
+  arr_prod *= arr2;
+  EXPECT_DOUBLE_EQ(arr_prod(0, 0), 5.0);
+  EXPECT_DOUBLE_EQ(arr_prod(0, 1), 12.0);
+  EXPECT_DOUBLE_EQ(arr_prod(1, 0), 21.0);
+  EXPECT_DOUBLE_EQ(arr_prod(1, 1), 32.0);
+
+  // Test operator/=
+  BaseArray<double> arr_div = arr2;
+  arr_div /= arr1;
+  EXPECT_DOUBLE_EQ(arr_div(0, 0), 5.0);
+  EXPECT_DOUBLE_EQ(arr_div(0, 1), 3.0);
+  EXPECT_DOUBLE_EQ(arr_div(1, 0), 7.0/3.0);
+  EXPECT_DOUBLE_EQ(arr_div(1, 1), 2.0);
+
+  // Test comparison
+  BaseArray<double> arr3 = arr1;
+  EXPECT_TRUE(arr1 == arr3);
+  arr3(0, 0) = 42.0;
+  EXPECT_FALSE(arr1 == arr3);
+}
+
+TEST(ControlArray1DTest, BasicOperations) {
+  xt::xarray<double> v1 = {1.0, 2.0, 3.0};
+  xt::xarray<double> v2 = {4.0, 5.0, 6.0};
+  ControlArray1D arr1d1(v1);
+  ControlArray1D arr1d2(v2);
+
+  // Test operator+=
+  ControlArray1D sum = arr1d1;
+  sum += arr1d2;
+  EXPECT_DOUBLE_EQ(sum(0), 5.0);
+  EXPECT_DOUBLE_EQ(sum(1), 7.0);
+  EXPECT_DOUBLE_EQ(sum(2), 9.0);
+
+  // Test operator-=
+  ControlArray1D diff = arr1d1;
+  diff -= arr1d2;
+  EXPECT_DOUBLE_EQ(diff(0), -3.0);
+  EXPECT_DOUBLE_EQ(diff(1), -3.0);
+  EXPECT_DOUBLE_EQ(diff(2), -3.0);
+
+  // Test operator*=
+  ControlArray1D prod = arr1d1;
+  prod *= arr1d2;
+  EXPECT_DOUBLE_EQ(prod(0), 4.0);
+  EXPECT_DOUBLE_EQ(prod(1), 10.0);
+  EXPECT_DOUBLE_EQ(prod(2), 18.0);
+
+  // Test operator/=
+  ControlArray1D div = arr1d2;
+  div /= arr1d1;
+  EXPECT_DOUBLE_EQ(div(0), 4.0);
+  EXPECT_DOUBLE_EQ(div(1), 2.5);
+  EXPECT_DOUBLE_EQ(div(2), 2.0);
+
+  // Test comparison
+  ControlArray1D arr3 = arr1d1;
+  EXPECT_TRUE(arr1d1 == arr3);
+  arr3(0) = 42.0;
+  EXPECT_FALSE(arr1d1 == arr3);
+}
+
 TEST(ControlArray1DTest, Construction1D) {
   xt::xarray<double> vec = {1.0, 2.0, 3.0};
   ControlArray1D     arr1d(vec);
