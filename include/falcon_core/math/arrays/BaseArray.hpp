f6ee9c7c8696145eded1b805243f81c6d91982f4
tjk144 <tkovach@wisc.edu>
2025-09-08 22:28:27 -0500

    Heres a BaseArray ported

--- include/falcon_core/math/arrays/BaseArray.hpp
+++ include/falcon_core/math/arrays/BaseArray.hpp
@@ -21,13 +21,136 @@ class BaseArray : public generic::Song {
  public:
   using MatrixType = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>;
 
-  BaseArray(const MatrixType &data);
-  BaseArray();
-
-  const MatrixType &data() const;
-  MatrixType       &data();
-
-  bool is_1d() const;
+  /**
+   * @brief Initializes an array object.
+   */
+  BaseArray(const MatrixType &data) : _data(data) {}
+  BaseArray() = default;
+  /**
+   * @brief Returns the underlying data in the array.
+   */
+  const MatrixType &data() const { return _data; }
+  /**
+   * @brief Returns the underlying data in the array.
+   */
+  MatrixType &data() { return _data; }
+  /**
+   * @brief Check if the data is equal to the other data.
+   * @param other The other data to compare to.
+   * @returnTrue if the data is equal, false otherwise.
+   */
+  bool operator==(const std::shared_ptr<BaseArray<T>> &other) const {
+    return data().isApprox(other->data());
+  }
+  /**
+   * @brief Add the data to the other data.
+   * @param other The other data to add.
+   * @return A new BaseArray with the added data.
+   */
+  std::shared_ptr<BaseArray<T>> operator+(
+      const std::shared_ptr<BaseArray<T>> &other) const {
+    return std::make_shared<BaseArray>(
+        BaseArray(data().array() + other->data().array()));
+  }
+  /**
+   * @brief Negates the data.
+   */
+  std::shared_ptr<BaseArray<T>> operator-() const {
+    return std::make_shared<BaseArray>(BaseArray(-data().array()));
+  }
+  /**
+   * @brief Subtracts the other data from to the data.
+   * @param other The other data to subtract.
+   * @return A new BaseArray with the subtracted data.
+   */
+  std::shared_ptr<BaseArray<T>> operator-(
+      const std::shared_ptr<BaseArray<T>> &other) const {
+    return std::make_shared<BaseArray>(
+        BaseArray(data().array() - other->data().array()));
+  }
+  /**
+   * @brief Raise the data to the power.
+   * @param power The power of the data to raise to.
+   * @return The data raised to the power.
+   */
+  std::shared_ptr<BaseArray<T>> operator^(const double &power) {
+    return std::make_shared<BaseArray>(BaseArray(data().array().pow(power)));
+  }
+  /**
+   * @brief Mutliply the data by the other data.
+   * @param other The other data to mulitply by.
+   * @returns The product of the data and the other data.
+   */
+  std::shared_ptr<BaseArray<T>> operator*(
+      const std::shared_ptr<BaseArray<T>> &other) const {
+    return std::make_shared<BaseArray>(
+        BaseArray(data().array() * other->data().array()));
+  }
+  /**
+   * @brief Divide the data by the other data.
+   * @param other The other data to divide by.
+   * @returns the data divided by the other data.
+   */
+  std::shared_ptr<BaseArray<T>> operator/(
+      const std::shared_ptr<BaseArray<T>> &other) const {
+    return std::make_shared<BaseArray>(
+        BaseArray(data().array() / other->data().array()));
+  }
+  /**
+   * @brief Return the sum of the squares of the data.
+   */
+  double get_sum_of_squares() { return (this ^ 2).array().sum(); }
+  /**
+   * @brief Return the sum of the squares of the data.
+   * @param other The numeric target to difference against.
+   */
+  double get_sum_of_squares(int other) {
+    return ((this - other) ^ 2).array().sum();
+  }
+  /**
+   * @brief Return the sum of the squares of the data.
+   * @param other The numeric target to difference against.
+   */
+  double get_sum_of_squares(double other) {
+    return ((this - other) ^ 2).array().sum();
+  }
+  /**
+   * @brief Return the sum of the squares of the data.
+   * @param other The numeric target to difference against.
+   */
+  double get_sum_of_squares(const std::shared_ptr<BaseArray<T>> &other) {
+    return ((this - *other) ^ 2).array().sum();
+  }
+  /**
+   * @brief Returns the absolute value of the array.
+   */
+  std::shared_ptr<BaseArray<T>> abs() const {
+    return std::make_shared<BaseArray>(BaseArray(this->data().array().abs()));
+  }
+  /**
+   * @brief Returns the min of the array.
+   */
+  T min() const { return this->data().array().minCoeff(); }
+  /**
+   * @brief Returns the minimum of the two arrays stacked.
+   */
+  std::shared_ptr<BaseArray<T>> min(
+      const std::shared_ptr<BaseArray<T>> &other) const {
+    return std::make_shared<BaseArray>(
+        BaseArray(this->data().min(other->data())));
+  }
+  /**
+   * @brief Returns the max of the array.
+   */
+  T max() const { return this->data().array().maxCoeff(); }
+  /**
+   * @brief Returns the maximum of the two arrays stacked.
+   */
+  std::shared_ptr<BaseArray<T>> max(
+      const std::shared_ptr<BaseArray<T>> &other) const {
+    return std::make_shared<BaseArray>(
+        BaseArray(this->data().max(other->data())));
+  }
 
  protected:
   MatrixType _data;
@@ -35,8 +158,12 @@ class BaseArray : public generic::Song {
  private:
   friend class cereal::access;
   template <class Archive>
-  void serialize(Archive &ar);
+  void serialize(Archive &ar) {
+    ar(cereal::base_class<generic::Song>(this), _data);
+  }
 };
+template <typename T>
+using BaseArraySP = std::shared_ptr<BaseArray<T>>;
 }  // namespace arrays
 }  // namespace math
 }  // namespace falcon_core
--- include/falcon_core/math/labelled_arrays/BaseLabelledArray.hpp
+++ include/falcon_core/math/labelled_arrays/BaseLabelledArray.hpp
@@ -9,9 +9,8 @@
 #include <memory>
 
 #include "falcon_core/autotuner_interfaces/contexts/AcquisitionContext.hpp"
-#include "falcon_core/generic/Song.hpp"
 #include "falcon_core/instrument_interfaces/names/InstrumentPort.hpp"
-
+#include "falcon_core/math/arrays/BaseArray.hpp"
 namespace falcon_core {
 namespace math {
 namespace labelled_arrays {
@@ -19,40 +18,48 @@ namespace labelled_arrays {
 /// @brief Associates an array with a label.
 /// @tparam ArrayType Type of the array.
 template <typename ArrayType>
-class BaseLabelledArray : public generic::Song {
-  std::shared_ptr<ArrayType>                                          _array;
-  std::shared_ptr<autotuner_interfaces::contexts::AcquisitionContext> _label;
+class BaseLabelledArray : public math::arrays::BaseArray<ArrayType> {
+  autotuner_interfaces::contexts::AcquisitionContextSP _label;
 
   friend class cereal::access;
   template <class Archive>
   void serialize(Archive& ar) {
-    ar(cereal::base_class<generic::Song>(this), _array, _label);
+    ar(cereal::base_class<math::arrays::BaseArray<ArrayType>>(this), _label);
   }
 
  public:
   BaseLabelledArray() = default;
+  BaseLabelledArray(math::arrays::BaseArraySP<ArrayType>                 array,
+                    autotuner_interfaces::contexts::AcquisitionContextSP label)
+      : _label(label), math::arrays::BaseArray<ArrayType>(array.data()) {}
   BaseLabelledArray(
-      std::shared_ptr<ArrayType>                                          array,
-      std::shared_ptr<autotuner_interfaces::contexts::AcquisitionContext> label)
-      : _array(array), _label(label) {}
-  BaseLabelledArray(
-      std::shared_ptr<ArrayType> array,
-      std::shared_ptr<falcon_core::instrument_interfaces::names::InstrumentPort>
-          label)
-      : _array(array),
+      math::arrays::BaseArraySP<ArrayType>                        array,
+      falcon_core::instrument_interfaces::names::InstrumentPortSP label)
+      : math::arrays::BaseArray<ArrayType>(array.data()),
         _label(std::make_shared<
                autotuner_interfaces::contexts::AcquisitionContext>(label)) {}
-
-  /**
-   * @brief Return the array.
-   */
-  const std::shared_ptr<ArrayType>& array() const { return _array; }
   /**
    * @brief Return the label.
    */
   const autotuner_interfaces::contexts::AcquisitionContextSP& label() const {
     return _label;
   }
+  /**
+   * @brief Return the connection.
+   */
+  const physics::device_structures::BaseConnection connection() const {
+    return label().connection();
+  }
+  /**
+   * @brief Return the instrument type.
+   */
+  const instrument_interfaces::Instrument instrument_type() const {
+    return label().instrument_type();
+  }
+  /**
+   * @brief Return the units.
+   */
+  const physics::units::SymbolUnit units() const { return label().units(); }
 };
 }  // namespace labelled_arrays
 }  // namespace math
