b240044bc1b6f7db128348b14981c3ee6c490ba2
Daniel Schug <daschug1@gmail.com>
2025-09-09 19:07:52 -0400

    refactor: update tests to use xt::xarray instead of Eigen for arrays

--- tests/test_arrays.cpp
+++ tests/test_arrays.cpp
@@ -1,41 +1,38 @@
 #include <gtest/gtest.h>
+#include <xtensor/xarray.hpp>
+#include <xtensor/xio.hpp>
 
 #include "falcon_core/math/arrays/ControlArray1D.hpp"
+#include "falcon_core/math/arrays/BaseArray.hpp"
 
 namespace tests {
 using namespace falcon_core::math::arrays;
 
 TEST(BaseArrayTest, ConstructionAndAccess) {
-  Eigen::MatrixXd mat(2, 2);
-  mat << 1.0, 2.0, 3.0, 4.0;
-  BaseArray<double> arr(mat);
-  EXPECT_EQ(arr.data().rows(), 2);
-  EXPECT_EQ(arr.data().cols(), 2);
-  EXPECT_DOUBLE_EQ(arr.data()(0, 0), 1.0);
-  EXPECT_DOUBLE_EQ(arr.data()(1, 1), 4.0);
+  xt::xarray<double> arr_data = {{1.0, 2.0}, {3.0, 4.0}};
+  BaseArray<double> arr(arr_data);
+  EXPECT_EQ(arr.shape()[0], 2);
+  EXPECT_EQ(arr.shape()[1], 2);
+  EXPECT_DOUBLE_EQ(arr(0, 0), 1.0);
+  EXPECT_DOUBLE_EQ(arr(1, 1), 4.0);
 }
 
 TEST(ControlArray1DTest, Construction1D) {
-  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> vec(1, 3);
-  vec << 1.0, 2.0, 3.0;
+  xt::xarray<double> vec = {1.0, 2.0, 3.0};
   ControlArray1D arr1d(vec);
-  EXPECT_EQ(arr1d.data().rows(), 1);
-  EXPECT_EQ(arr1d.data().cols(), 3);
-  EXPECT_DOUBLE_EQ(arr1d.data()(0, 2), 3.0);
+  EXPECT_EQ(arr1d.shape()[0], 3);
+  EXPECT_EQ(arr1d.dimension(), 1);
+  EXPECT_DOUBLE_EQ(arr1d(2), 3.0);
 }
 
 TEST(ControlArray1DTest, ThrowsOnNon1D) {
-  Eigen::MatrixXd mat(2, 2);
-  mat << 1.0, 2.0, 3.0, 4.0;
-  // Use a lambda to avoid "most vexing parse" and ensure exception is thrown on
-  // construction
+  xt::xarray<double> mat = {{1.0, 2.0}, {3.0, 4.0}};
   EXPECT_THROW({ ControlArray1D arr(mat); }, std::invalid_argument);
 }
 
 TEST(BaseArrayTest, SerializationRoundTrip) {
-  Eigen::MatrixXd mat(2, 2);
-  mat << 1.0, 2.0, 3.0, 4.0;
-  BaseArray<double> arr(mat);
+  xt::xarray<double> arr_data = {{1.0, 2.0}, {3.0, 4.0}};
+  BaseArray<double> arr(arr_data);
 
   // Serialize to JSON using the class helper
   std::string json = arr.to_json_string();
@@ -44,15 +41,14 @@ TEST(BaseArrayTest, SerializationRoundTrip) {
   // Deserialize from JSON using the class helper
   auto arr2 = BaseArray<double>::from_json_string<BaseArray<double>>(json);
 
-  EXPECT_EQ(arr2->data().rows(), 2);
-  EXPECT_EQ(arr2->data().cols(), 2);
-  EXPECT_DOUBLE_EQ(arr2->data()(0, 0), 1.0);
-  EXPECT_DOUBLE_EQ(arr2->data()(1, 1), 4.0);
+  EXPECT_EQ(arr2->shape()[0], 2);
+  EXPECT_EQ(arr2->shape()[1], 2);
+  EXPECT_DOUBLE_EQ((*arr2)(0, 0), 1.0);
+  EXPECT_DOUBLE_EQ((*arr2)(1, 1), 4.0);
 }
 
 TEST(ControlArray1DTest, SerializationRoundTrip) {
-  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> vec(1, 3);
-  vec << 1.0, 2.0, 3.0;
+  xt::xarray<double> vec = {1.0, 2.0, 3.0};
   auto arr1d = std::make_shared<ControlArray1D>(vec);
 
   // Serialize to JSON using the class helper
@@ -62,8 +58,8 @@ TEST(ControlArray1DTest, SerializationRoundTrip) {
   // Deserialize from JSON using the class helper
   auto arr1d2 = ControlArray1D::from_json_string<ControlArray1D>(json);
 
-  EXPECT_EQ(arr1d2->data().rows(), 1);
-  EXPECT_EQ(arr1d2->data().cols(), 3);
-  EXPECT_DOUBLE_EQ(arr1d2->data()(0, 2), 3.0);
+  EXPECT_EQ(arr1d2->shape()[0], 3);
+  EXPECT_EQ(arr1d2->dimension(), 1);
+  EXPECT_DOUBLE_EQ((*arr1d2)(2), 3.0);
 }
 }  // namespace tests
