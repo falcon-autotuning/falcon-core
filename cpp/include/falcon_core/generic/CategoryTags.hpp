05e8af00d1ac7b919e192853a44393c5478857f6
Daniel Schug <daschug1@gmail.com>
2025-10-30 20:13:34 -0400

    fix: List now supports booleans

--- cpp/include/falcon_core/generic/CategoryTags.hpp
+++ cpp/include/falcon_core/generic/CategoryTags.hpp
@@ -6,6 +6,7 @@ namespace generic {
 namespace category {
 struct song_tag {};
 struct primitive_tag {};
+struct bool_tag {};
 struct other_tag {};
 
 template <typename T>
@@ -14,6 +15,11 @@ struct determine_tag {
       std::is_base_of<Song, T>::value && !is_primitive<T>::value,
       song_tag,
       std::conditional_t<is_primitive<T>::value, primitive_tag, other_tag>>;
+};
+template <typename T>
+struct determine_bool_tag {
+  using type =
+      std::conditional_t<std::is_same<T, bool>::value, bool_tag, other_tag>;
 };
 }  // namespace category
 }  // namespace generic
