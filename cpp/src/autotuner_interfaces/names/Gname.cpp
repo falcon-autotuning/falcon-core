3dc7c6e32ce099c6ce0d7c2edded2ce9fe6bb601
Daniel Schug <daschug1@gmail.com>
2025-11-03 20:47:43 -0500

    fix: Simplify Gname constructor to resolve compilation error

--- cpp/src/autotuner_interfaces/names/Gname.cpp
+++ cpp/src/autotuner_interfaces/names/Gname.cpp
@@ -5,16 +5,9 @@ namespace autotuner_interfaces {
 namespace names {
 Gname::Gname() : NameBase() {}
 Gname::Gname(const std::string& name)
-    : Gname(name.rfind("group", 0) == 0
-                ? Gname(std::stoi(name.substr(std::string("group").length())))
-                : Gname()) {
-  if (name.rfind("group", 0) != 0) {
-    // This is a hack to allow for non-standard gnames.
-    // The delegating constructor calls the default constructor,
-    // then we overwrite the name.
-    const_cast<std::string&>(this->name()) = name;
-  }
-}
+    : NameBase(name.rfind("group", 0) == 0
+                   ? NameBase(name, "group")
+                   : NameBase(name)) {}
 Gname::Gname(const int& num) : NameBase(num, "group") {}
 std::string Gname::gname() const { return name(); }
 bool        Gname::operator==(const Gname& other) const {
