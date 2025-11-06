447560f86c618da17c864052d3f97e781117a3f8
Daniel Schug <daschug1@gmail.com>
2025-11-06 00:25:44 -0500

    feat: AxesDiscretizer tested c-api

--- cpp/src/math/Axes.cpp
+++ cpp/src/math/Axes.cpp
@@ -5,10 +5,14 @@
 #include <cereal/types/polymorphic.hpp>
 
 #include "falcon_core/instrument_interfaces/names/InstrumentPort.hpp"
+#include "falcon_core/math/discrete_spaces/Discretizer.hpp"
 #include "falcon_core/math/domains/CoupledLabelledDomain.hpp"
+
 CEREAL_REGISTER_TYPE(falcon_core::math::Axes<int>)
 CEREAL_REGISTER_TYPE(falcon_core::math::Axes<double>)
 CEREAL_REGISTER_TYPE(
     falcon_core::math::Axes<falcon_core::math::domains::CoupledLabelledDomain>)
 CEREAL_REGISTER_TYPE(falcon_core::math::Axes<
                      falcon_core::instrument_interfaces::names::InstrumentPort>)
+CEREAL_REGISTER_TYPE(
+    falcon_core::math::Axes<falcon_core::math::discrete_spaces::Discretizer>)
