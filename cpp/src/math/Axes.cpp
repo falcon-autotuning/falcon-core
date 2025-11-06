9d06fc268111ba1e658ca8a097770071021fe8e6
Daniel Schug <daschug1@gmail.com>
2025-11-06 01:53:47 -0500

    feat: AxesLabelledControlArray tested c-api (maybe)

--- cpp/src/math/Axes.cpp
+++ cpp/src/math/Axes.cpp
@@ -7,6 +7,7 @@
 #include "falcon_core/instrument_interfaces/names/InstrumentPort.hpp"
 #include "falcon_core/math/arrays/ControlArray.hpp"
 #include "falcon_core/math/arrays/ControlArray1D.hpp"
+#include "falcon_core/math/arrays/LabelledControlArray.hpp"
 #include "falcon_core/math/discrete_spaces/Discretizer.hpp"
 #include "falcon_core/math/domains/CoupledLabelledDomain.hpp"
 
@@ -22,3 +23,5 @@ CEREAL_REGISTER_TYPE(
     falcon_core::math::Axes<falcon_core::math::arrays::ControlArray>)
 CEREAL_REGISTER_TYPE(
     falcon_core::math::Axes<falcon_core::math::arrays::ControlArray1D>)
+CEREAL_REGISTER_TYPE(
+    falcon_core::math::Axes<falcon_core::math::arrays::LabelledControlArray>)
