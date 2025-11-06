667ad07d6ab3eb0a6ad09e23beee05b99a102bb5
tjk144 <tkovach@wisc.edu>
2025-11-06 12:20:04 -0600

    C-api coverage is up to 83.1%

--- cpp/src/generic/Pair.cpp
+++ cpp/src/generic/Pair.cpp
@@ -1,5 +1,8 @@
 #include "falcon_core/generic/Pair.hpp"
 
+#include "falcon_core/autotuner_interfaces/names/Channel.hpp"
+#include "falcon_core/physics/device_structures/Connections.hpp"
+
 CEREAL_REGISTER_TYPE(falcon_core::generic::Pair<std::string, std::string>);
 CEREAL_REGISTER_TYPE(falcon_core::generic::Pair<std::string, double>);
 CEREAL_REGISTER_TYPE(falcon_core::generic::Pair<std::string, bool>);
@@ -8,6 +11,9 @@ CEREAL_REGISTER_TYPE(falcon_core::generic::Pair<float, float>);
 CEREAL_REGISTER_TYPE(falcon_core::generic::Pair<int, int>);
 CEREAL_REGISTER_TYPE(falcon_core::generic::Pair<int, float>);
 CEREAL_REGISTER_TYPE(falcon_core::generic::Pair<size_t, size_t>);
+CEREAL_REGISTER_TYPE(falcon_core::generic::Pair<
+                     falcon_core::autotuner_interfaces::names::Channel,
+                     falcon_core::physics::device_structures::Connections>);
 
 using PSS = falcon_core::generic::Pair<std::string, std::string>;
 CEREAL_REGISTER_POLYMORPHIC_RELATION(falcon_core::generic::Song, PSS);
@@ -23,3 +29,7 @@ using PII = falcon_core::generic::Pair<int, int>;
 CEREAL_REGISTER_POLYMORPHIC_RELATION(falcon_core::generic::Song, PII);
 using PIF = falcon_core::generic::Pair<int, float>;
 CEREAL_REGISTER_POLYMORPHIC_RELATION(falcon_core::generic::Song, PIF);
+using PCC = falcon_core::generic::Pair<
+    falcon_core::autotuner_interfaces::names::Channel,
+    falcon_core::physics::device_structures::Connections>;
+CEREAL_REGISTER_POLYMORPHIC_RELATION(falcon_core::generic::Song, PCC);
