210c4c797a61739ec95cf9de680646afa0e7a299
tjk144 <tkovach@wisc.edu>
2025-11-12 16:08:48 -0600

    Removed unimportant folders

--- dist/python/tests/conftest.py
+++
@@ -1,5 +0,0 @@
-import falcon_core
-
-__all__ = [
-    "falcon_core",
-]
--- dist/python/tests/test_list.py
+++
@@ -1,6 +0,0 @@
-from falcon_core.generic.list import List
-
-
-def test_serialization():
-    list = List.create_empty()
-    assert list.empty()
