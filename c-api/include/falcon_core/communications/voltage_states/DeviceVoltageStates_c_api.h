7ac17ee7af6c0c3fd560a641b920488c490729cc
tjk144 <tkovach@wisc.edu>
2026-01-23 17:43:50 -0600

    Fixed the __declspec for all of the autotuner+interfaces in teh c-api

--- c-api/include/falcon_core/communications/voltage_states/DeviceVoltageStates_c_api.h
+++ c-api/include/falcon_core/communications/voltage_states/DeviceVoltageStates_c_api.h
@@ -11,80 +11,92 @@ extern "C" {
 typedef void* DeviceVoltageStatesHandle;
 
 // @category:allocation
-DeviceVoltageStatesHandle DeviceVoltageStates_create_empty();
+FALCON_CORE_C_API DeviceVoltageStatesHandle DeviceVoltageStates_create_empty();
 // @category:allocation
-DeviceVoltageStatesHandle DeviceVoltageStates_create(
-    ListDeviceVoltageStateHandle items);
+FALCON_CORE_C_API DeviceVoltageStatesHandle
+DeviceVoltageStates_create(ListDeviceVoltageStateHandle items);
 // @category:deallocation
-void DeviceVoltageStates_destroy(DeviceVoltageStatesHandle handle);
+FALCON_CORE_C_API void DeviceVoltageStates_destroy(
+    DeviceVoltageStatesHandle handle);
 // @category:read
-/* AUTO-DOC from cpp: DeviceVoltageStates_states | falcon_core::communications::voltage_states::DeviceVoltageStates::states */
+/* AUTO-DOC from cpp: DeviceVoltageStates_states |
+ * falcon_core::communications::voltage_states::DeviceVoltageStates::states */
 /**
  * @brief Returns the list of voltage states.
  */
-ListDeviceVoltageStateHandle DeviceVoltageStates_states(
-    DeviceVoltageStatesHandle handle);
+FALCON_CORE_C_API ListDeviceVoltageStateHandle
+DeviceVoltageStates_states(DeviceVoltageStatesHandle handle);
 // @category:write
-/* AUTO-DOC from cpp: DeviceVoltageStates_add_state | falcon_core::communications::voltage_states::DeviceVoltageStates::add_state */
+/* AUTO-DOC from cpp: DeviceVoltageStates_add_state |
+ * falcon_core::communications::voltage_states::DeviceVoltageStates::add_state
+ */
 /**
  * @brief Add a voltage state to the collection.
  * @param state The voltage state to add.
  */
-void DeviceVoltageStates_add_state(DeviceVoltageStatesHandle handle,
-                                   DeviceVoltageStateHandle  state);
+FALCON_CORE_C_API void DeviceVoltageStates_add_state(
+    DeviceVoltageStatesHandle handle, DeviceVoltageStateHandle state);
 // @category:read
-/* AUTO-DOC from cpp: DeviceVoltageStates_find_state | falcon_core::communications::voltage_states::DeviceVoltageStates::find_state */
+/* AUTO-DOC from cpp: DeviceVoltageStates_find_state |
+ * falcon_core::communications::voltage_states::DeviceVoltageStates::find_state
+ */
 /**
  * @brief Find a voltage state by its connection.
  * @param connection The connection to find.
  * @returns the voltage state if found.
  */
-DeviceVoltageStatesHandle DeviceVoltageStates_find_state(
+FALCON_CORE_C_API DeviceVoltageStatesHandle DeviceVoltageStates_find_state(
     DeviceVoltageStatesHandle handle, ConnectionHandle connection);
 // @category:read
-/* AUTO-DOC from cpp: DeviceVoltageStates_to_point | falcon_core::communications::voltage_states::DeviceVoltageStates::to_point */
+/* AUTO-DOC from cpp: DeviceVoltageStates_to_point |
+ * falcon_core::communications::voltage_states::DeviceVoltageStates::to_point */
 /**
  * @brief Conversion to the Point datatype.
  */
-PointHandle DeviceVoltageStates_to_point(DeviceVoltageStatesHandle handle);
+FALCON_CORE_C_API PointHandle
+DeviceVoltageStates_to_point(DeviceVoltageStatesHandle handle);
 // @category:read
-DeviceVoltageStatesHandle DeviceVoltageStates_intersection(
+FALCON_CORE_C_API DeviceVoltageStatesHandle DeviceVoltageStates_intersection(
     DeviceVoltageStatesHandle handle, DeviceVoltageStatesHandle other);
 // @category:write
-void DeviceVoltageStates_push_back(DeviceVoltageStatesHandle handle,
-                                   DeviceVoltageStateHandle  value);
+FALCON_CORE_C_API void DeviceVoltageStates_push_back(
+    DeviceVoltageStatesHandle handle, DeviceVoltageStateHandle value);
 // @category:read
-size_t DeviceVoltageStates_size(DeviceVoltageStatesHandle handle);
+FALCON_CORE_C_API size_t
+DeviceVoltageStates_size(DeviceVoltageStatesHandle handle);
 // @category:read
-bool DeviceVoltageStates_empty(DeviceVoltageStatesHandle handle);
+FALCON_CORE_C_API bool DeviceVoltageStates_empty(
+    DeviceVoltageStatesHandle handle);
 // @category:write
-void DeviceVoltageStates_erase_at(DeviceVoltageStatesHandle handle, size_t idx);
-// @category:write
-void DeviceVoltageStates_clear(DeviceVoltageStatesHandle handle);
-// @category:read
-DeviceVoltageStateHandle DeviceVoltageStates_at(
+FALCON_CORE_C_API void DeviceVoltageStates_erase_at(
     DeviceVoltageStatesHandle handle, size_t idx);
-// @category:read
-ListDeviceVoltageStateHandle DeviceVoltageStates_items(
+// @category:write
+FALCON_CORE_C_API void DeviceVoltageStates_clear(
     DeviceVoltageStatesHandle handle);
 // @category:read
-bool DeviceVoltageStates_contains(DeviceVoltageStatesHandle handle,
-                                  DeviceVoltageStateHandle  value);
+FALCON_CORE_C_API DeviceVoltageStateHandle
+DeviceVoltageStates_at(DeviceVoltageStatesHandle handle, size_t idx);
 // @category:read
-size_t DeviceVoltageStates_index(DeviceVoltageStatesHandle handle,
-                                 DeviceVoltageStateHandle  value);
+FALCON_CORE_C_API ListDeviceVoltageStateHandle
+DeviceVoltageStates_items(DeviceVoltageStatesHandle handle);
 // @category:read
-bool DeviceVoltageStates_equal(DeviceVoltageStatesHandle a,
-                               DeviceVoltageStatesHandle b);
+FALCON_CORE_C_API bool DeviceVoltageStates_contains(
+    DeviceVoltageStatesHandle handle, DeviceVoltageStateHandle value);
 // @category:read
-bool DeviceVoltageStates_not_equal(DeviceVoltageStatesHandle a,
-                                   DeviceVoltageStatesHandle b);
+FALCON_CORE_C_API size_t DeviceVoltageStates_index(
+    DeviceVoltageStatesHandle handle, DeviceVoltageStateHandle value);
 // @category:read
-StringHandle DeviceVoltageStates_to_json_string(
-    DeviceVoltageStatesHandle handle);
+FALCON_CORE_C_API bool DeviceVoltageStates_equal(DeviceVoltageStatesHandle a,
+                                                 DeviceVoltageStatesHandle b);
+// @category:read
+FALCON_CORE_C_API bool DeviceVoltageStates_not_equal(
+    DeviceVoltageStatesHandle a, DeviceVoltageStatesHandle b);
+// @category:read
+FALCON_CORE_C_API StringHandle
+DeviceVoltageStates_to_json_string(DeviceVoltageStatesHandle handle);
 // @category:allocation
-DeviceVoltageStatesHandle DeviceVoltageStates_from_json_string(
-    StringHandle json);
+FALCON_CORE_C_API DeviceVoltageStatesHandle
+DeviceVoltageStates_from_json_string(StringHandle json);
 
 #ifdef __cplusplus
 }
