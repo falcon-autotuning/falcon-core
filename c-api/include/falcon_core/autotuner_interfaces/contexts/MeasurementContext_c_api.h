d90c5b765ccf753a5afe9b0942d4cbc2e2a28ebc
tjk144 <tkovach@wisc.edu>
2026-01-22 18:25:00 -0600

    Started adding windows macros the the c-api

--- c-api/include/falcon_core/autotuner_interfaces/contexts/MeasurementContext_c_api.h
+++ c-api/include/falcon_core/autotuner_interfaces/contexts/MeasurementContext_c_api.h
@@ -4,37 +4,42 @@ extern "C" {
 #endif
 #include <stdbool.h>
 
+#include "falcon_core/export_c_api.h"
 #include "falcon_core/generic/String_c_api.h"
 #include "falcon_core/instrument_interfaces/names/InstrumentPort_c_api.h"
 #include "falcon_core/physics/device_structures/Connection_c_api.h"
 typedef void* MeasurementContextHandle;
 
 // @category:allocation
-MeasurementContextHandle MeasurementContext_copy(
-    MeasurementContextHandle handle);
+FALCON_CORE_C_API MeasurementContextHandle
+MeasurementContext_copy(MeasurementContextHandle handle);
 // @category:deallocation
-void MeasurementContext_destroy(MeasurementContextHandle handle);
+FALCON_CORE_C_API void MeasurementContext_destroy(
+    MeasurementContextHandle handle);
 // @category:read
-bool MeasurementContext_equal(MeasurementContextHandle handle,
-                              MeasurementContextHandle other);
+FALCON_CORE_C_API bool MeasurementContext_equal(MeasurementContextHandle handle,
+                                                MeasurementContextHandle other);
 // @category:read
-bool MeasurementContext_not_equal(MeasurementContextHandle handle,
-                                  MeasurementContextHandle other);
+FALCON_CORE_C_API bool MeasurementContext_not_equal(
+    MeasurementContextHandle handle, MeasurementContextHandle other);
 // @category:read
-StringHandle MeasurementContext_to_json_string(MeasurementContextHandle handle);
+FALCON_CORE_C_API StringHandle
+MeasurementContext_to_json_string(MeasurementContextHandle handle);
 // @category:allocation
-MeasurementContextHandle MeasurementContext_from_json_string(StringHandle json);
+FALCON_CORE_C_API MeasurementContextHandle
+MeasurementContext_from_json_string(StringHandle json);
 // @category:allocation
-MeasurementContextHandle MeasurementContext_create(
+FALCON_CORE_C_API MeasurementContextHandle MeasurementContext_create(
     ConnectionHandle connection, StringHandle instrument_type);
 // @category:allocation
-MeasurementContextHandle MeasurementContext_create_from_port(
-    InstrumentPortHandle port);
+FALCON_CORE_C_API MeasurementContextHandle
+MeasurementContext_create_from_port(InstrumentPortHandle port);
 // @category:read
-ConnectionHandle MeasurementContext_connection(MeasurementContextHandle handle);
+FALCON_CORE_C_API ConnectionHandle
+MeasurementContext_connection(MeasurementContextHandle handle);
 // @category:read
-StringHandle MeasurementContext_instrument_type(
-    MeasurementContextHandle handle);
+FALCON_CORE_C_API StringHandle
+MeasurementContext_instrument_type(MeasurementContextHandle handle);
 
 #ifdef __cplusplus
 }
--- c-api/include/falcon_core/autotuner_interfaces/names/Channel_c_api.h
+++ c-api/include/falcon_core/autotuner_interfaces/names/Channel_c_api.h
@@ -4,26 +4,28 @@ extern "C" {
 #endif
 #include <stdbool.h>
 
+#include "falcon_core/export_c_api.h"
 #include "falcon_core/generic/String_c_api.h"
 
 typedef void* ChannelHandle;
 
 // @category:allocation
-ChannelHandle Channel_copy(ChannelHandle handle);
+FALCON_CORE_C_API ChannelHandle Channel_copy(ChannelHandle handle);
 // @category:deallocation
-void Channel_destroy(ChannelHandle handle);
+FALCON_CORE_C_API void Channel_destroy(ChannelHandle handle);
 // @category:read
-bool Channel_equal(ChannelHandle handle, ChannelHandle other);
+FALCON_CORE_C_API bool Channel_equal(ChannelHandle handle, ChannelHandle other);
 // @category:read
-bool Channel_not_equal(ChannelHandle handle, ChannelHandle other);
+FALCON_CORE_C_API bool Channel_not_equal(ChannelHandle handle,
+                                         ChannelHandle other);
 // @category:read
-StringHandle Channel_to_json_string(ChannelHandle handle);
+FALCON_CORE_C_API StringHandle Channel_to_json_string(ChannelHandle handle);
 // @category:allocation
-ChannelHandle Channel_from_json_string(StringHandle json);
+FALCON_CORE_C_API ChannelHandle Channel_from_json_string(StringHandle json);
 // @category:allocation
-ChannelHandle Channel_create(StringHandle name);
+FALCON_CORE_C_API ChannelHandle Channel_create(StringHandle name);
 // @category:read
-StringHandle Channel_name(ChannelHandle handle);
+FALCON_CORE_C_API StringHandle Channel_name(ChannelHandle handle);
 
 #ifdef __cplusplus
 }
