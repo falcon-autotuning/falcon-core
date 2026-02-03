1cc686621293d8cd7fd43d5c03777538d8a83543
tjk144 <tkovach@wisc.edu>
2026-02-03 12:50:37 -0600

    Updated the CI workflow to include cross platform integration tests and include building windows for cpp and c-api

--- c-api/src/falcon_core/generic/ListImpedance_c_api.cpp
+++ c-api/src/falcon_core/generic/ListImpedance_c_api.cpp
@@ -1,5 +1,5 @@
-#include <falcon_core\generic/List.hpp>
-#include "falcon_core\generic\ListImpedance_c_api.h"
+#include <falcon_core/generic/List.hpp>
+#include "falcon_core/generic/ListImpedance_c_api.h"
 #include "falcon_core/Precompiled_c_api.h"
 #include "falcon_core/export_c_api.h"
 #include <falcon_core/physics/device_structures/Impedance.hpp>
